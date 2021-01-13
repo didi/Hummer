9cb6f4455b15b076e8a3bedd731d5723325692f4
ChasonTang <chasontang@warmbloom.com>
2021-01-13 20:24:24 +0800

    Feature/issue 33 (#35)
    * refactor(iOS): Refactor engine abstract class
    
    1. Remove unused HMBaseExecutor HMBaseWeakValue
    2. HMBaseExecutor name property change to optional
    3. HMBaseValue executor property change to nullable
    
    * chore(iOS): Add missing file
    
    * feat(iOS): Implement HMBaseValue
    
    1. Change HMClosureType to HMFunctionType
    2. Add HMBaseExecutorProtocol private field
    3. Add HMBaseValueProtocol private field
    4. Implement HMBaseValue

---
+++ iOS/Hummer/Classes/Engine/HMBaseExecutorProtocol.h
@@ -0,0 +1,135 @@
+//
+//  HMBaseExecutor.h
+//  Hummer
+//
+//  Created by 唐佳诚 on 2020/8/3.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol HMBaseValueProtocol;
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * 如果是原生给 JS 的闭包，由 JS 调用，参数为 NSArray<id<HMBaseValueProtocol>> *，返回值为 id
+ * 如果是 JS 给原生的闭包，由原生调用，参数为 NSArray *，返回值为 id<HMBaseValueProtocol>
+ * 如果是原生给 JS 的闭包，由原生调用，参数为 NSArray *，返回值为 id，不考虑
+ * 如果是 JS 给原生的闭包，由 JS 调用，参数为 NSArray<id<HMBaseValueProtocol>> *，返回值为 id<HMBaseValueProtocol>，不考虑
+ * 未来需要探测 block 参数
+ * 并支持任意类型参数
+ *
+ * 注意：如果闭包不返回任何值，会导致崩溃
+ */
+typedef id _Nullable (^HMFunctionType)(NSArray *_Nullable value);
+
+// 兼容以前代码
+typedef HMFunctionType HMFuncCallback;
+
+@class HMExceptionModel;
+
+@protocol HMBaseExecutorProtocol
+
+@required
+
+@property (nonatomic, readonly, strong) id <HMBaseValueProtocol> globalObject;
+
+@property (nonatomic, copy, nullable) void (^exceptionHandler)(HMExceptionModel *exception);
+
++ (nullable id <HMBaseExecutorProtocol>)currentContext;
+
++ (nullable NSArray<id <HMBaseValueProtocol>> *)currentArguments;
+
+- (nullable id <HMBaseValueProtocol>)evaluateScript:(nullable NSString *)script withSourceURL:(nullable NSURL *)sourceURL;
+
+/// 使用下标获取属性
+/// @param key 可以为 id<HMBaseValueProtocol>，也可以为字符串，当前只支持字符串，如果支持 Number 等，改成 id<NSCopying>
+- (nullable id <HMBaseValueProtocol>)objectForKeyedSubscript:(id)key;
+
+- (void)setObject:(nullable id)object forKeyedSubscript:(id)key;
+
+#pragma mark - 私有代码 业务方请勿使用
+
+#pragma mark - 类型判断
+
+/// nil 参数被认为是 JavaScript undefined
+
+- (BOOL)valueIsNullOrUndefined:(nullable id <HMBaseValueProtocol>)value;
+
+- (BOOL)valueIsBoolean:(nullable id <HMBaseValueProtocol>)value;
+
+/// 布尔类型不是数字
+/// @param value id<HMBaseValueProtocol>
+- (BOOL)valueIsNumber:(nullable id <HMBaseValueProtocol>)value;
+
+/// new String() 是对象不是标量
+/// @param value id<HMBaseValueProtocol>
+- (BOOL)valueIsString:(nullable id <HMBaseValueProtocol>)value;
+
+/// 单纯判断是否为 JavaScript Object
+/// @param value id<HMBaseValueProtocol>
+- (BOOL)valueIsObject:(nullable id <HMBaseValueProtocol>)value DEPRECATED_MSG_ATTRIBUTE("兼容 JavaScriptCore 需要，废弃接口，业务方实际上需要的是判断是否为字典");
+
+/// 包括 Proxy 代理的 Array
+/// @param value id<HMBaseValueProtocol>
+- (BOOL)valueIsArray:(nullable id <HMBaseValueProtocol>)value;
+
+#pragma mark - 私有方法 业务方请勿调用
+
+#pragma mark - 新增类型判断
+
+- (BOOL)valueIsDictionary:(nullable id <HMBaseValueProtocol>)value DEPRECATED_MSG_ATTRIBUTE("兼容 JavaScriptCore 需要，废弃接口，直接转换成 dictionary + 判空");
+
+- (BOOL)valueIsNativeObject:(nullable id <HMBaseValueProtocol>)value;
+
+- (BOOL)valueIsFunction:(nullable id <HMBaseValueProtocol>)value;
+
+#pragma mark - Native -> JavaScriptCore
+
+- (nullable id <HMBaseValueProtocol>)convertToValueWithObject:(nullable id)object;
+
+- (nullable id <HMBaseValueProtocol>)convertToValueWithNumber:(nullable NSNumber *)number;
+
+#pragma mark - JavaScriptCore -> Native
+
+- (nullable id)convertToObjectWithValue:(nullable id <HMBaseValueProtocol>)value isPortableConvert:(BOOL)isPortableConvert;
+
+- (nullable NSNumber *)convertToNumberWithValue:(nullable id <HMBaseValueProtocol>)value;
+
+- (nullable NSString *)convertToStringWithValue:(nullable id <HMBaseValueProtocol>)value;
+
+- (nullable NSArray *)convertToArrayWithValue:(nullable id <HMBaseValueProtocol>)value isPortableConvert:(BOOL)isPortableConvert;
+
+- (nullable NSDictionary<NSString *, id> *)convertToDictionaryWithValue:(nullable id <HMBaseValueProtocol>)value isPortableConvert:(BOOL)isPortableConvert;
+
+- (nullable NSObject *)convertToNativeObjectWithValue:(nullable id <HMBaseValueProtocol>)value;
+
+- (nullable HMFunctionType)convertToFunctionWithValue:(nullable id <HMBaseValueProtocol>)value;
+
+#pragma mark - Misc
+
+- (BOOL)compareWithValue:(nullable id <HMBaseValueProtocol>)value anotherValue:(nullable id <HMBaseValueProtocol>)anotherValue;
+
+- (nullable id <HMBaseValueProtocol>)callWithValue:(nullable id <HMBaseValueProtocol>)value arguments:(nullable NSArray *)arguments;
+
+- (nullable id <HMBaseValueProtocol>)invokeMethodWithValue:(nullable id <HMBaseValueProtocol>)value method:(NSString *)method withArguments:(nullable NSArray *)arguments;
+
+- (nullable id <HMBaseValueProtocol>)getWithValue:(nullable id <HMBaseValueProtocol>)value propertyName:(nullable NSString *)propertyName;
+
+- (void)setWithValue:(nullable id <HMBaseValueProtocol>)value propertyName:(nullable NSString *)propertyName propertyObject:(nullable id)propertyObject;
+
+@optional
+
+/// 这个只有 JavaScriptCore 才有
+@property (nonatomic, nullable, copy) NSString *name;
+
+/**
+ * 下面两个先不实现，暂时不满足
+ */
+- (nullable id <HMBaseValueProtocol>)objectAtIndexedSubscript:(NSUInteger)idx;
+
+- (void)setObject:(nullable id)obj atIndexedSubscript:(NSUInteger)idx;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- iOS/Hummer/Classes/Engine/HMBaseValue.m
+++ iOS/Hummer/Classes/Engine/HMBaseValue.m
@@ -6,3 +6,202 @@
 //
 
 #import "HMBaseValue.h"
+#import "HMLogger.h"
+
+static NSString *const KeyIsNotString = @"HMBaseValue - setObject:forKeyedSubscript:/objectForKeyedSubscript: key is not string";
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HMBaseValue ()
+
+@property (nonatomic, weak, nullable) id <HMBaseExecutorProtocol> executor;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+@implementation HMBaseValue
+
+- (BOOL)isUndefined {
+    return [self.executor valueIsNullOrUndefined:self];
+}
+
+- (BOOL)isNull {
+    return [self.executor valueIsNullOrUndefined:self];
+}
+
+- (BOOL)isBoolean {
+    return [self.executor valueIsBoolean:self];
+}
+
+- (BOOL)isNumber {
+    return [self.executor valueIsNumber:self];
+}
+
+- (BOOL)isString {
+    return [self.executor valueIsString:self];
+}
+
+- (BOOL)isObject {
+    return [self.executor valueIsObject:self];
+}
+
+- (BOOL)isArray {
+    return [self.executor valueIsArray:self];
+}
+
++ (nullable id <HMBaseValueProtocol>)valueWithObject:(id)value inContext:(id <HMBaseExecutorProtocol>)context {
+    return [context convertToValueWithObject:value];
+}
+
++ (nullable id <HMBaseValueProtocol>)valueWithBool:(BOOL)value inContext:(id <HMBaseExecutorProtocol>)context {
+    return [context convertToValueWithNumber:@(value)];
+}
+
++ (nullable id <HMBaseValueProtocol>)valueWithDouble:(double)value inContext:(id <HMBaseExecutorProtocol>)context {
+    return [context convertToValueWithNumber:@(value)];
+}
+
++ (nullable id <HMBaseValueProtocol>)valueWithInt32:(int32_t)value inContext:(id <HMBaseExecutorProtocol>)context {
+    return [context convertToValueWithNumber:@(value)];
+}
+
++ (nullable id <HMBaseValueProtocol>)valueWithUInt32:(uint32_t)value inContext:(id <HMBaseExecutorProtocol>)context {
+    return [context convertToValueWithNumber:@(value)];
+}
+
+- (BOOL)isDictionary {
+    return [self.executor valueIsDictionary:self];
+}
+
+- (BOOL)isNativeObject {
+    return [self.executor valueIsNativeObject:self];
+}
+
+- (BOOL)isFunction {
+    return [self.executor valueIsFunction:self];
+}
+
++ (nullable id <HMBaseValueProtocol>)valueWithNullInContext:(id)context {
+    return nil;
+}
+
++ (nullable id <HMBaseValueProtocol>)valueWithUndefinedInContext:(id)context {
+    return nil;
+}
+
+- (nullable id)toObject {
+    return [self.executor convertToObjectWithValue:self isPortableConvert:NO];
+}
+
+- (BOOL)toBool {
+    return self.toNumber.boolValue;
+}
+
+- (double)toDouble {
+    return self.toNumber.doubleValue;
+}
+
+- (int32_t)toInt32 {
+    return self.toNumber.intValue;
+}
+
+- (uint32_t)toUInt32 {
+    return self.toNumber.unsignedIntValue;
+}
+
+- (nullable NSNumber *)toNumber {
+    return [self.executor convertToNumberWithValue:self];
+}
+
+- (nullable NSString *)toString {
+    return [self.executor convertToStringWithValue:self];
+}
+
+- (nullable NSArray *)toArray {
+    return [self.executor convertToArrayWithValue:self isPortableConvert:NO];
+}
+
+- (nullable NSDictionary<NSString *, id> *)toDictionary {
+    return [self.executor convertToDictionaryWithValue:self isPortableConvert:NO];
+}
+
+- (nullable id)toPortableObject {
+    return [self.executor convertToObjectWithValue:self isPortableConvert:YES];
+}
+
+- (nullable NSDictionary<NSString *, NSObject *> *)toPortableDictionary {
+    return [self.executor convertToDictionaryWithValue:self isPortableConvert:YES];
+}
+
+- (nullable NSArray<NSObject *> *)toPortableArray {
+    return [self.executor convertToArrayWithValue:self isPortableConvert:YES];
+}
+
+- (nullable NSObject *)toNativeObject {
+    return [self.executor convertToNativeObjectWithValue:self];
+}
+
+- (BOOL)hm_isArray {
+    return self.isArray;
+}
+
+- (nullable NSArray *)hm_toObjcArray {
+    return self.toArray;
+}
+
+- (nullable id)hm_toObjCObject {
+    id object = self.toNativeObject;
+    if (!object) {
+        object = self.toObject;
+    }
+
+    return object;
+}
+
+- (nullable HMFunctionType)toFunction {
+    return [self.executor convertToFunctionWithValue:self];
+}
+
+- (BOOL)isEqualToObject:(id)other {
+    return [self.executor compareWithValue:self anotherValue:other];
+}
+
+- (nullable id <HMBaseValueProtocol>)callWithArguments:(NSArray *)arguments {
+    return [self.executor callWithValue:self arguments:arguments];
+}
+
+- (nullable id <HMBaseValueProtocol>)invokeMethod:(NSString *)method withArguments:(NSArray *)arguments {
+    return [self.executor invokeMethodWithValue:self method:method withArguments:arguments];
+}
+
+- (nullable id <HMBaseValueProtocol>)objectForKeyedSubscript:(id)key {
+    if (![key isKindOfClass:NSString.class]) {
+        HMLogError(KeyIsNotString);
+
+        return nil;
+    }
+
+    return [self.executor getWithValue:self propertyName:key];
+}
+
+- (void)setObject:(id)object forKeyedSubscript:(id)key {
+    if (![key isKindOfClass:NSString.class]) {
+        HMLogError(KeyIsNotString);
+
+        return;
+    }
+
+    [self.executor setWithValue:self propertyName:key propertyObject:object];
+}
+
+- (instancetype)initWithExecutor:(id)executor {
+    if (!executor) {
+        return nil;
+    }
+    self = [super init];
+    _executor = executor;
+
+    return self;
+}
+
+@end
