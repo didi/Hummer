4fe4c232bcbd44856e014fca3e831298338c2923
ChasonTang <chasontang@warmbloom.com>
2021-01-13 21:51:54 +0800

    chore: Temp commit

--- iOS/Hummer/Classes/Engine/JavaScriptCore/HMJSCExecutor.m
+++ iOS/Hummer/Classes/Engine/JavaScriptCore/HMJSCExecutor.m
@@ -9,6 +9,20 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+static void hummerFinalize(JSObjectRef object);
+
+static JSValueRef hummerCall(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef *exception);
+
+static JSValueRef hummerCreate(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef *exception);
+
+static JSValueRef hummerCallFunction(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef *exception);
+
+static JSValueRef hummerGetProperty(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef *exception);
+
+static JSValueRef hummerSetProperty(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef *exception);
+
+static JSContextGroupRef _Nullable virtualMachineRef = NULL;
+
 @interface HMJSCExecutor ()
 
 @property (nonatomic, assign) JSGlobalContextRef contextRef;
@@ -25,6 +39,42 @@ NS_ASSUME_NONNULL_END
     if (!HMExecutorMap) {
         HMExecutorMap = NSMapTable.strongToWeakObjectsMapTable;
     }
+    if (!virtualMachineRef) {
+        virtualMachineRef = JSContextGroupCreate();
+    }
+    _contextRef = JSGlobalContextCreateInGroup(virtualMachineRef, NULL);
+    [HMExecutorMap setObject:self forKey:[NSValue valueWithPointer:_contextRef]];
+    
+    // 注入对象
+    JSStringRef hummerCallString = JSStringCreateWithUTF8CString("hummerCall");
+    JSStringRef hummerCreateString = JSStringCreateWithUTF8CString("hummerCreate");
+    JSStringRef hummerGetPropertyString = JSStringCreateWithUTF8CString("hummerGetProperty");
+    JSStringRef hummerSetPropertyString = JSStringCreateWithUTF8CString("hummerSetProperty");
+    JSStringRef hummerCallFunctionString = JSStringCreateWithUTF8CString("hummerCallFunction");
+    JSObjectRef globalThis = JSContextGetGlobalObject(_contextRef);
+    
+    // 匿名函数
+    JSObjectRef hummerCallFunction = JSObjectMakeFunctionWithCallback(_contextRef, NULL, &hummerCall);
+    JSObjectRef hummerCreateFunction = JSObjectMakeFunctionWithCallback(_contextRef, NULL, &hummerCreate);
+    JSObjectRef hummerGetPropertyFunction = JSObjectMakeFunctionWithCallback(_contextRef, NULL, &hummerGetProperty);
+    JSObjectRef hummerSetPropertyFunction = JSObjectMakeFunctionWithCallback(_contextRef, NULL, &hummerSetProperty);
+    JSObjectRef hummerCallFunctionFunction = JSObjectMakeFunctionWithCallback(_contextRef, NULL, &hummerCallFunction);
+    JSValueRef exception = NULL;
+    JSObjectSetProperty(_contextRef, globalThis, hummerCallString, hummerCallFunction, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, &exception);
+    [self popExceptionWithErrorObject:&exception];
+    JSObjectSetProperty(_contextRef, globalThis, hummerCreateString, hummerCreateFunction, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, &exception);
+    [self popExceptionWithErrorObject:&exception];
+    JSObjectSetProperty(_contextRef, globalThis, hummerGetPropertyString, hummerGetPropertyFunction, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, &exception);
+    [self popExceptionWithErrorObject:&exception];
+    JSObjectSetProperty(_contextRef, globalThis, hummerSetPropertyString, hummerSetPropertyFunction, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, &exception);
+    [self popExceptionWithErrorObject:&exception];
+    JSObjectSetProperty(_contextRef, globalThis, hummerCallFunctionString, hummerCallFunctionFunction, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, &exception);
+    [self popExceptionWithErrorObject:&exception];
+    JSStringRelease(hummerCallString);
+    JSStringRelease(hummerCreateString);
+    JSStringRelease(hummerGetPropertyString);
+    JSStringRelease(hummerSetPropertyString);
+    JSStringRelease(hummerCallFunctionString);
 
     return self;
 }
