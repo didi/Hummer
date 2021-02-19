df1c268f504b83b60abe5f660c09b2c4968170a5
ChasonTang <chasontang@warmbloom.com>
2021-02-19 17:51:34 +0800

    chore(iOS): Temp commit

--- iOS/builtin/lib.d.ts
+++
@@ -1,8 +0,0 @@
-declare class View { viewID?: string }
-
-declare interface Timer {
-    setInterval(handler: () => void, timeout?: number);
-    clearInterval();
-    setTimeout(handler: () => void, timeout?: number);
-    clearTimeout();
-}
--- iOS/builtin/src/index.ts
+++ iOS/builtin/src/index.ts
@@ -1,3 +1,12 @@
+declare class View { viewID?: string }
+
+declare interface Timer {
+    setInterval(handler: () => void, timeout?: number): void;
+    clearInterval(): void;
+    setTimeout(handler: () => void, timeout?: number): void;
+    clearTimeout(): void;
+}
+
 interface MethodPropertyModel {
     readonly nameString?: string,
     readonly isClass?: boolean,
@@ -6,8 +15,22 @@ interface MethodPropertyModel {
 
 interface LoaderClassModel {
     readonly className?: string,
-    readonly methodPropertyList?: [MethodPropertyModel],
+    readonly methodPropertyList?: (MethodPropertyModel | null | undefined)[],
     readonly superClassName?: string,
+}
+
+function isOfType<T>(varToBeChecked: unknown, propertyToCheckFor: keyof T): varToBeChecked is T {
+    return (varToBeChecked as T)[propertyToCheckFor] !== undefined;
+}
+
+if (globalThis.nativeLoggingHook) {
+    const originalConsole = globalThis.console;
+    if (originalConsole) {
+        const descriptor = Object.getOwnPropertyDescriptor(globalThis, 'console');
+        if (descriptor) {
+            Object.defineProperty(globalThis, 'originalConsole', descriptor);
+        }
+    }
 }
 
 globalThis.__GLOBAL__ = globalThis;
@@ -44,7 +67,7 @@ globalThis.hummerValueStorageDelete = (value: unknown) => {
     }
 }
 
-function isString(value: unknown) {
+function isString(value: unknown): value is string {
     return typeof value === 'string' && value.length > 0;
 }
 
@@ -55,35 +78,53 @@ class HummerBase extends Object {
 
     constructor(...args: unknown[]) {
         super();
-        let className = this.constructor.name;
-        if (isString(this.constructor[exportedClassNameCache]) && globalThis.exporedComponentMap?.get(this.constructor[exportedClassNameCache])) {
-            // 直接使用缓存类名
-            className = this.constructor[exportedClassNameCache];
-            if (Object.getPrototypeOf(this.constructor.prototype).constructor[exportedClassNameCache] && Object.prototype.hasOwnProperty.call(this, exportedClassNameCache)) {
-                // 原型链上已经缓存并且自身也重复缓存则删除自身缓存
-                delete this.constructor[exportedClassNameCache];
-            }
-        } else if (!globalThis.exporedComponentMap?.get(className)) {
-            // 不是导出组件
+        let className = this.constructor[exportedClassNameCache]
+        // 1. 已缓存
+        //   1. 已注入
+        //   2. 未注入（兜底错误处理）实际上基本不可能存在
+        // 2. 未缓存
+        //   1. 本身就是注入组件
+        //   2. 不是注入组件，则需要查找原型链，查找最近的属于注入组件的类名
+        //     1. 查找成功，缓存
+        //     2. 查找失败（兜底错误逻辑）比如 new HummerBase() 或者 class TestA extends HummerBase {}
+        // 已注入 === globalThis 存在该类/函数
+        // 当 globalThis.X = class X extends View {} 的情况的时候，会走到 2.1 的情况，并且直接把 X 当做注入原生组件的 JS 类名
+        // 所以绝对禁止上述写法，业务方类不能放到全局声明
+        if (typeof className === 'string' && !globalThis[className]) {
+            console.error(`${className} 已缓存未注入`)
+
+            return;
+        }
+
+        if (typeof className !== 'string') {
             let prototype = Object.getPrototypeOf(this);
-            // 结束条件 1 找到导出组件
-            // 结束条件 2 prototype === HummerBase.prototype
-            while (!globalThis.exporedComponentMap?.get(className) && prototype !== HummerBase.prototype) {
-                prototype = Object.getPrototypeOf(prototype);
-                className = prototype.constructor.name;
+            // new HummerBase() 情况需要排除，不能进入循环
+            if (prototype !== HummerBase.prototype) {
+                // 结束条件 1 找到导出组件
+                // 结束条件 2 prototype === HummerBase.prototype
+                do {
+                    className = prototype.constructor.name;
+                    prototype = Object.getPrototypeOf(prototype);
+                } while (globalThis[className] || prototype == HummerBase.prototype)
             }
-            // 找到了导出组件
-            if (prototype === HummerBase.prototype || !isString(className)) {
-                // 排除特殊情况
-                console.error('兜底断言触发');
+            // 缓存
+            if (typeof className === 'string' && globalThis[className]) {
+                this.constructor[exportedClassNameCache] = className;
+            } else {
+                // 兜底错误
+                console.error('兜底异常，大概率为 new HummerBase() 或者 class TestA extends HummerBase {}')
 
                 return;
             }
-            // 缓存
-            this.constructor[exportedClassNameCache] = className;
         }
-        // 构造 _private
+
         this._private = globalThis.hummerCreate(className, this, ...args);
+        if (!this._private) {
+            console.error('兜底异常，大概率为 globalThis.X = class X extends View {} 全局声明业务类的情况')
+
+            return;
+        }
+
         // 兼容代码
         this.initialize(...args);
     }
@@ -112,7 +153,9 @@ globalThis.hummerCreateObject = (privatePointer: unknown, jsClassName: string) =
     return newObject;
 }
 
-globalThis.hummerLoadClass = (loaderModel?: Record<string, LoaderClassModel | undefined>) => {
+// 由于 Object.keys 会返回 string[]，因此不需要考虑 number 和 symbol 情况
+// LoaderClassModel 本身就可以是一个 {}，因此 只需要额外考虑 undefined 和 null 的情况
+globalThis.hummerLoadClass = (loaderModel?: Record<string, LoaderClassModel | undefined | null>) => {
     if (!loaderModel) {
         // 提示错误信息
         console.error('hummerLoaderClass 空调用');
@@ -121,72 +164,53 @@ globalThis.hummerLoadClass = (loaderModel?: Record<string, LoaderClassModel | un
     }
 
     Object.keys(loaderModel).forEach(jsClassName => {
-        interface StackModel {
-            jsClassName: string,
-            value: LoaderClassModel,
-        }
-        let stackArray: Array<StackModel> | undefined;
-
-        let value = loaderModel[jsClassName];
-        if (!isString(jsClassName) || globalThis.exporedComponentMap?.get(jsClassName) || !value) {
+        const loaderClassModel = loaderModel[jsClassName];
+        if (globalThis[jsClassName] || !loaderClassModel) {
             return;
         }
-        do {
-            if (!value) {
-                return;
-            }
+        function injectModel(loaderClassModel: LoaderClassModel, loaderModel: Record<string, LoaderClassModel | undefined | null>): typeof HummerBase | undefined {
             let jsClass = HummerBase;
-            if (value.superClassName && isString(value.superClassName) && loaderModel[value.superClassName]) {
-                // 有父类
-                if (!globalThis.exporedComponentMap?.get(value.superClassName)) {
-                    // 父类未加载，则递归加载
-                    if (!stackArray) {
-                        stackArray = [];
+            if (isString(loaderClassModel.superClassName) && loaderModel[loaderClassModel.superClassName] && !globalThis[loaderClassModel.superClassName]) {
+                // 有父类并且没有注入则先注入父类
+                const innerLoaderClassModel = loaderModel[loaderClassModel.superClassName]
+                if (innerLoaderClassModel) {
+                    const superClass = injectModel(innerLoaderClassModel, loaderModel)
+                    if (superClass) {
+                        jsClass = superClass
                     }
-                    // 压栈
-                    stackArray.push({
-                        jsClassName,
-                        value
-                    });
-                    jsClassName = value.superClassName;
-                    value = loaderModel[jsClassName];
-                    // 下一个循环
-                    continue;
-                } else {
-                    // 正常加载父类
-                    jsClass = globalThis.exporedComponentMap?.get(value.superClassName);
                 }
             }
             jsClass = class extends jsClass { };
-            value.methodPropertyList?.forEach(value => {
-                if (!value.nameString || !isString(value.nameString)) {
+            loaderClassModel.methodPropertyList?.forEach(methodPropertyModel => {
+                if (!methodPropertyModel || !isString(methodPropertyModel.nameString)) {
                     return;
                 }
                 // 由于栈帧恢复，所以需要使用新变量绑定原变量
                 const inlineJSClassName = jsClassName;
-                const prototypeOrClsss = value.isClass ? jsClass : jsClass?.prototype;
-                if (value.isMethod) {
-                    prototypeOrClsss[value.nameString] = function (...args: unknown[]) {
-                        if (value.isClass) {
-                            return globalThis.hummerCall(inlineJSClassName, value.nameString, ...args);
+                const prototypeOrClsss = methodPropertyModel.isClass ? jsClass : jsClass.prototype;
+                if (methodPropertyModel.isMethod) {
+                    prototypeOrClsss[methodPropertyModel.nameString] = function (...args: unknown[]) {
+                        if (isOfType<HummerBase>(this, '_private')) {
+                            return globalThis.hummerCall(this._private, inlineJSClassName, methodPropertyModel.nameString, ...args);
                         } else {
-                            return globalThis.hummerCall((this as HummerBase)._private, inlineJSClassName, value.nameString, ...args);
+                            return globalThis.hummerCall(inlineJSClassName, methodPropertyModel.nameString, ...args);
                         }
                     }
                 } else {
-                    Object.defineProperty(prototypeOrClsss, value.nameString, {
-                        get: function () {
-                            if (value.isClass) {
-                                return globalThis.hummerGetProperty(inlineJSClassName, value.nameString);
+                    Object.defineProperty(prototypeOrClsss, methodPropertyModel.nameString, {
+                        // 未能自动推断出来的 this 类型，最好加 unknown，保证 call apply 等情况
+                        get: function (this: HummerBase | unknown) {
+                            if (isOfType<HummerBase>(this, '_private')) {
+                                return globalThis.hummerGetProperty(this._private, inlineJSClassName, methodPropertyModel.nameString);
                             } else {
-                                return globalThis.hummerGetProperty(this._private, inlineJSClassName, value.nameString);
+                                return globalThis.hummerGetProperty(inlineJSClassName, methodPropertyModel.nameString);
                             }
                         },
-                        set: function (newValue: unknown) {
-                            if (value.isClass) {
-                                return globalThis.hummerSetProperty(inlineJSClassName, value.nameString, newValue);
+                        set: function (this: HummerBase | unknown, newValue: unknown) {
+                            if (isOfType<HummerBase>(this, '_private')) {
+                                return globalThis.hummerSetProperty(this._private, inlineJSClassName, methodPropertyModel.nameString, newValue);
                             } else {
-                                return globalThis.hummerSetProperty(this._private, inlineJSClassName, value.nameString, newValue);
+                                return globalThis.hummerSetProperty(inlineJSClassName, methodPropertyModel.nameString, newValue);
                             }
                         }
                     })
@@ -194,22 +218,12 @@ globalThis.hummerLoadClass = (loaderModel?: Record<string, LoaderClassModel | un
             })
             // writable: false, enumerable: false, configurable: true                
             Object.defineProperty(jsClass, 'name', { value: jsClassName });
-            // 挂载类
-            if (!globalThis.exporedComponentMap) {
-                globalThis.exporedComponentMap = new Map();
-            }
-            globalThis.exporedComponentMap.set(jsClassName, jsClass);
+            // 挂载
             globalThis[jsClassName] = jsClass;
-            if (stackArray && stackArray.length == 0) {
-                stackArray = undefined;
-            }
-            const stackTop = stackArray?.pop();
-            if (stackTop) {
-                // 恢复栈帧
-                jsClassName = stackTop.jsClassName;
-                value = stackTop.value;
-            }
-        } while (stackArray)
+
+            return jsClass;
+        }
+        injectModel(loaderClassModel, loaderModel)
     });
 
     if (globalThis.Hummer && !globalThis.Hummer.notifyCenter) {
@@ -225,6 +239,7 @@ globalThis.hummerLoadClass = (loaderModel?: Record<string, LoaderClassModel | un
     }
 
     globalThis.clearInterval = timer => {
+        // 强制类型转换
         (timer as unknown as Timer).clearInterval();
         globalThis.hummerValueStorageDelete(timer);
     }
@@ -234,7 +249,10 @@ globalThis.hummerLoadClass = (loaderModel?: Record<string, LoaderClassModel | un
         globalThis.hummerValueStorageAdd(timer);
         timer.setTimeout(() => {
             globalThis.hummerValueStorageDelete(timer);
-            (handler as () => void)();
+            // 去除 string 调用全局函数的情况
+            if (handler instanceof Function) {
+                handler();
+            }
         }, timeout);
 
         return timer;
