08c719f36cd3ff41e7e1d440638fef235dde815c
zhangdanfeng <zhangdanfeng@didiglobal.com>
2021-02-23 10:39:35 +0800

    feat(Android): 修改动画组件的repeatCount属性定义，和iOS对齐；新增repeatMode属性

--- android/hummer-sdk/src/main/java/com/didi/hummer/render/component/anim/BasicAnimation.java
+++ android/hummer-sdk/src/main/java/com/didi/hummer/render/component/anim/BasicAnimation.java
@@ -4,6 +4,8 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.text.TextUtils;
 
 import com.didi.hummer.annotation.Component;
 import com.didi.hummer.annotation.JsMethod;
@@ -29,10 +31,25 @@ public class BasicAnimation {
     protected float duration; //单位(s)
     @JsProperty("delay")
     protected float delay; //单位(s)
-    @JsProperty("repeatCount")
-    protected int repeatCount;
     @JsProperty("easing")
     protected String easing;
+    /**
+     * 动画重复次数（默认是1）
+     *
+     * <0: 无限次
+     * 0、1: 动画做1次
+     * 2: 动画做2次
+     */
+    @JsProperty("repeatCount")
+    protected int repeatCount;
+    /**
+     * 动画重复模式（默认是'normal'）
+     *
+     * 'normal': 正向重复，如：[0->1] [0->1] [0->1]
+     * 'reverse': 反向重复，如：[0->1] [1->0] [0->1]
+     */
+    @JsProperty("repeatMode")
+    protected String repeatMode;
 
     protected Animator animator;
     protected String animType;
@@ -52,11 +69,9 @@ public class BasicAnimation {
     public static final int DIRECTION_Y = 12;
     public static final int DIRECTION_XY = 13;
 
-
     protected static Object[] trans2Array(Object value) {
         return HummerAnimationUtils.trans2Array(value);
     }
-
 
     @JsMethod("on")
     public void on(String event, JSCallback callback) {
@@ -73,7 +88,8 @@ public class BasicAnimation {
 
         animator = anim;
         anim.setDuration(HummerAnimationUtils.getAnimDuration(duration));
-        anim.setRepeatCount(repeatCount);
+        anim.setRepeatCount(toRawRepeatCount(repeatCount));
+        anim.setRepeatMode(toRawRepeatMode(repeatMode));
         anim.setStartDelay(HummerAnimationUtils.getAnimDelay(delay));
         anim.setInterpolator(HummerAnimationUtils.getInterpolator(easing));
         anim.addListener(animatorListener);
@@ -95,6 +111,41 @@ public class BasicAnimation {
 
     public boolean isRunning() {
         return animator != null && animator.isRunning();
+    }
+
+    /**
+     * 转换成Android原生的repeatCount
+     *
+     * 由于Hummer中定义的repeatCount和Android原生的repeatCount不一致，所以这里原先转换一下
+     *
+     * @return
+     */
+    private int toRawRepeatCount(int repeatCount) {
+        int rawRepeatCount = 0;
+        if (repeatCount < 0) {
+            rawRepeatCount = ValueAnimator.INFINITE;
+        } else if (repeatCount > 1) {
+            rawRepeatCount = repeatCount - 1;
+        }
+        return rawRepeatCount;
+    }
+
+    /**
+     * 转换成Android原生的repeatMode
+     *
+     * @return
+     */
+    private int toRawRepeatMode(String repeatMode) {
+        int rawRepeatMode = ValueAnimator.RESTART;
+        if (!TextUtils.isEmpty(repeatMode)) {
+            repeatMode = repeatMode.toLowerCase();
+            if (repeatMode.equals("normal")) {
+                rawRepeatMode = ValueAnimator.RESTART;
+            } else if (repeatMode.equals("reverse")) {
+                rawRepeatMode = ValueAnimator.REVERSE;
+            }
+        }
+        return rawRepeatMode;
     }
 
     protected AnimatorListenerAdapter animatorListener = new AnimatorListenerAdapter() {
