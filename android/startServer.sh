31388dfdb294e6b30df6530930c8df1d5a4f1610
XiaoFeng <bbssyyuui@qq.com>
2021-03-14 14:19:28 +0800

    chore(Android): 移除内部hummer-test工程，使用外部examples工程

--- android/startServer.sh
+++ android/startServer.sh
@@ -1,11 +1,6 @@
 echo "######## start hummer local server ########"
 
 work_path=$(dirname $0)
-server_dir=$work_path/../test/hummer
-
-if [[ ! -d "$server_dir" ]]; then
-  server_dir=$work_path/hummer-test
-fi
-
+server_dir=$work_path/../examples/hummer
 cd $server_dir
 hummer dev
