7ecd60757542c9659faa3930e35894a3e0ae9ba8
XiaoFeng <bbssyyuui@qq.com>
2021-04-11 20:25:42 +0800

    feat(Android): 整理示例页面

---
+++ android/hummer-demo-app/src/main/java/com/didi/hummer/demo/HummerEmbeddedActivity.java
@@ -0,0 +1,114 @@
+package com.didi.hummer.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.didi.hummer.HummerRender;
+import com.didi.hummer.context.HummerContext;
+import com.didi.hummer.core.engine.JSValue;
+import com.didi.hummer.render.style.HummerLayout;
+
+/**
+ * Hummer嵌入式页面示例
+ *
+ * Created by XiaoFeng on 2020-01-02.
+ */
+public class HummerEmbeddedActivity extends AppCompatActivity {
+
+    private HummerRender hmRender1;
+    private HummerRender hmRender2;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hummer_embedded);
+
+        renderLayout1();
+        renderLayout2();
+    }
+
+    private void renderLayout1() {
+        HummerLayout layout1 = findViewById(R.id.layout_hummer_1);
+        hmRender1 = new HummerRender(layout1, "test_namespace");
+        // 页面加载回调（可选）
+        hmRender1.setRenderCallback(new HummerRender.HummerRenderCallback() {
+            @Override
+            public void onSucceed(HummerContext hmContext, JSValue jsPage) {
+
+            }
+
+            @Override
+            public void onFailed(Exception e) {
+
+            }
+        });
+
+        // 渲染Hummer页面
+        hmRender1.renderWithUrl("http://xxx.xxx.xxx.xxx:8000/HelloWorld.js");
+    }
+
+    private void renderLayout2() {
+        HummerLayout layout2 = findViewById(R.id.layout_hummer_2);
+        hmRender2 = new HummerRender(layout2, "test_namespace");
+        // 页面加载回调（可选）
+        hmRender2.setRenderCallback(new HummerRender.HummerRenderCallback() {
+            @Override
+            public void onSucceed(HummerContext hmContext, JSValue jsPage) {
+
+            }
+
+            @Override
+            public void onFailed(Exception e) {
+
+            }
+        });
+
+        // 渲染Hummer页面
+        hmRender2.renderWithUrl("http://xxx.xxx.xxx.xxx:8000/index.js");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (hmRender1 != null) {
+            hmRender1.onResume();
+        }
+        if (hmRender2 != null) {
+            hmRender2.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (hmRender1 != null) {
+            hmRender1.onPause();
+        }
+        if (hmRender2 != null) {
+            hmRender2.onPause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (hmRender1 != null) {
+            hmRender1.onDestroy();
+        }
+        if (hmRender2 != null) {
+            hmRender2.onDestroy();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (hmRender1 != null && hmRender1.onBack()) {
+            return;
+        }
+        if (hmRender2 != null && hmRender2.onBack()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+}
--- android/hummer-demo-app/src/main/java/com/didi/hummer/demo/HummerPageFragmentActivity.java
+++ android/hummer-demo-app/src/main/java/com/didi/hummer/demo/HummerPageFragmentActivity.java
@@ -11,15 +11,17 @@ import com.didi.hummer.adapter.navigator.NavPage;
 import com.didi.hummer.adapter.navigator.impl.DefaultNavigatorAdapter;
 
 /**
+ * Hummer单页面示例
+ *
  * Created by XiaoFeng on 2020-01-02.
  */
-public class MyHummerFragmentActivity extends FragmentActivity {
+public class HummerPageFragmentActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_fragment);
+        setContentView(R.layout.activity_page_fragment);
 
         NavPage page = null;
         try {
@@ -27,7 +29,7 @@ public class MyHummerFragmentActivity extends FragmentActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        loadFragment(MyHummerFragment.newInstance(page), R.id.fragment_container, "fragment_hummer");
+        loadFragment(HummerPageFragment.newInstance(page), R.id.fragment_container, "fragment_hummer");
     }
 
     private void loadFragment(Fragment fragment, int fragmentContainerId, String tag) {
