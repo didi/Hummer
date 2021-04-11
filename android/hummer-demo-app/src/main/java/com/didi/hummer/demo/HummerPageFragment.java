c514ba491355d6d88ecb5dcce1bbbac9f002856f
XiaoFeng <bbssyyuui@qq.com>
2021-04-11 20:25:42 +0800

    feat(Android): 整理示例页面

--- android/hummer-demo-app/src/main/java/com/didi/hummer/demo/HummerPageFragment.java
+++ android/hummer-demo-app/src/main/java/com/didi/hummer/demo/HummerPageFragment.java
@@ -1,10 +1,12 @@
 package com.didi.hummer.demo;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.didi.hummer.HummerActivity;
+import com.didi.hummer.HummerFragment;
 import com.didi.hummer.adapter.navigator.NavPage;
+import com.didi.hummer.adapter.navigator.impl.DefaultNavigatorAdapter;
 import com.didi.hummer.context.HummerContext;
 import com.didi.hummer.core.engine.JSCallback;
 import com.didi.hummer.core.engine.JSValue;
@@ -18,15 +20,18 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by XiaoFeng on 2020-01-02.
+ * Hummer单页面示例
+ *
+ * Created by XiaoFeng on 2020/9/21.
  */
-public class MyHummerActivity extends HummerActivity {
+public class HummerPageFragment extends HummerFragment {
 
-    @Override
-    protected NavPage getPageInfo() {
-        return super.getPageInfo();
-//        return new NavPage("HelloWorld6.js");
-//        return new NavPage("/sdcard/test/HelloWorld6.js");
+    public static HummerFragment newInstance(@NonNull NavPage page) {
+        HummerPageFragment fragment = new HummerPageFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(DefaultNavigatorAdapter.EXTRA_PAGE_MODEL, page);
+        fragment.setArguments(args);
+        return fragment;
     }
 
 //    @Override
@@ -87,10 +92,10 @@ public class MyHummerActivity extends HummerActivity {
         });
 
         // NotifyCenter全局通知消息
-//        NotifyCenter.addEventListener("testEvent", new NotifyCallback(this) {
+//        NotifyCenter.addEventListener("testEvent", new NotifyCallback(mHMContext.getJsContext()) {
 //            @Override
 //            public void onNotify(Object event) {
-//                Log.v("zdf", "onNotify, event = " + event);
+//                Log.v("zdf", "onNotify, obj = " + obj);
 //            }
 //        });
 //
