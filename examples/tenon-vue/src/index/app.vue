4fa76e54ff8151b4381e898dd653c51c20889f47
duanlikang <duanlikang@didichuxing.com>
2021-04-20 18:08:18 +0800

    doc: add tenon demo

---
+++ examples/tenon-vue/src/index/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <view style="height:100%;background-color:#ffcccc">
+    <text>Hello Tenon</text>
+  </view>
+</template>
+<style lang="less" scoped>
+</style>
+
+<script>
+export default {
+  data(){
+    return {
+    }
+  }
+}
+</script>
