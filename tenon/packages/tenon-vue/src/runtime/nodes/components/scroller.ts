bf485dbd4a28d9f7e38f546645e4fee0fdc9f5d2
duanlikang <duanlikang@didichuxing.com>
2021-05-07 17:30:06 +0800

    chore: modify tenon comment

--- tenon/packages/tenon-vue/src/runtime/nodes/components/scroller.ts
+++ tenon/packages/tenon-vue/src/runtime/nodes/components/scroller.ts
@@ -17,7 +17,7 @@ export class Scroller extends Base{
         break;
       case 'scroll-direction':
         if(value === 'horizontal' &&  this.element instanceof ScrollerComponent){
-          // 属性切换时，滚动插件需要重新生命；同时元素渲染从children一次向上，这个时候element children已经赋值
+          // 属性切换时，Scroller组件需要重新声明，同时进行 Children的重新赋值
           let scroller = new HorizontalScroller() as any
           scroller.style = this._style
           for(let child of this.children.values()){
