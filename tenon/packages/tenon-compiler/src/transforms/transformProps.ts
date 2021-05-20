e19f00139deccc09cea192abe2bd5c7d2037d855
chenyulun <308813800@qq.com>
2021-05-20 12:07:32 +0800

    fix(tenon): 添加条件判断属性转换驼峰

--- tenon/packages/tenon-compiler/src/transforms/transformProps.ts
+++ tenon/packages/tenon-compiler/src/transforms/transformProps.ts
@@ -1,16 +1,19 @@
 import { NodeTransform, NodeTypes} from '@vue/compiler-core'
-import { camelize } from '@hummer/tenon-utils'
+import { camelize, isNativeTags } from '@hummer/tenon-utils'
 // 处理内置组件的参数和事件
 export const transformProps:NodeTransform = (node:any) => {
   return function(){
-    if(node.type === NodeTypes.ELEMENT){
-      const { props } = node
+    const { props, tag, type } = node
+    if(type === NodeTypes.ELEMENT){
       props.forEach((p: any) => {
-        if(p.type === NodeTypes.ATTRIBUTE) {
+        //FIXME: 自定义ex-开头的组件也支持驼峰设置的时候删除isNativeTags判断
+        if(isNativeTags(tag)) {
+          if(p.type === NodeTypes.ATTRIBUTE) {
             p.name = camelize(p.name)
-        }
-        if(p.type === NodeTypes.DIRECTIVE && p.name === 'bind') {
-            p.arg.content = camelize(p.arg.content)
+          }
+          if(p.type === NodeTypes.DIRECTIVE && p.name === 'bind') {
+              p.arg.content = camelize(p.arg.content)
+          }
         }
         if(p.type === NodeTypes.DIRECTIVE && p.name === 'on' && p.arg.content === 'click') {
             p.arg.content = 'tap'
