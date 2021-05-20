67578d5388b8b41cd0680ab61df1466c05fd2064
duanlikang <duanlikang@didichuxing.com>
2021-05-20 15:07:13 +0800

    fix: 修复 border 缩写与预期不符的问题

--- tenon/packages/tenon-utils/src/style/transformer/border.ts
+++ tenon/packages/tenon-utils/src/style/transformer/border.ts
@@ -1,10 +1,6 @@
+import {isBorderStyle, isLength, Style} from '../common/utils'
 import {extend} from '../../index'
 const attrs = ["border","border-left", "border-right", "border-top", "border-bottom"]
-const borderBreakReadius = /([\.\w]+)\s+(\w+)\s+([\w#\s\(,\.\)]+)/
-interface Style {
-  attr: string,
-  value: string
-}
 
 export function transformBorder(style:Record<string, string>):Record<string, string>{
   let tempStyle:Record<string, string> = {
@@ -15,33 +11,33 @@ export function transformBorder(style:Record<string, string>):Record<string, str
       return;
     }else {
       delete tempStyle[attr]
-      tempStyle = extend(transformBorderStyle({
-        attr: attr,
-        value: style[attr]
-      }), tempStyle)
+      tempStyle = extend(transformBorderStyle(attr, style[attr]), tempStyle)
     }
   })
   return tempStyle
 }
 
 /**
- * 转换Border Style
- * @param value string
+ * 转换Border Style，进行聚合属性的拆解
+ * 参考 MDN 文档：https://developer.mozilla.org/en-US/docs/Web/CSS/border
+ * The border property may be specified using one, two, or three of the values listed below. The order of the values does not matter.
+ * @param attr 待拆解的属性
+ * @param value 待拆解的属性值
  * ex.borderLeft: 1px solid #eee; borderWidth: 1px; borderStyle: solid; borderColor: #eee;
+ * ex.border: none; borderStyle: none;
  */
-function transformBorderStyle({
-  attr,
-  value
-}:Style){
-  let matches = value.match(borderBreakReadius)
-  // 如果不符合标准Border写法，返回为空
-  if(!matches){
-    return {}
+function transformBorderStyle(attr:string, borderValue:string){
+  let values = borderValue.trim().split(/\s+/);
+  let tempStyle:Style = {};
+  for(let i=0,len=values.length; i<len; i++){
+    if(isBorderStyle(values[i])){
+      tempStyle[attr + "-style"] = values[i]
+    }else if(isLength(values[i])){
+      tempStyle[attr + "-width"] = values[i]
+    }else{
+      tempStyle[attr + "-color"] = values[i]
+    }
   }
-  let [,borderWidth, borderStyle, borderColor] = matches
-  return {
-    [attr+"-width"]: borderWidth,
-    [attr+"-style"]: borderStyle,
-    [attr+"-color"]: borderColor,
-  }
+  return tempStyle
 }
+
