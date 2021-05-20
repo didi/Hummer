d5fa6534ada63569bfc8711c231a385646f20944
duanlikang <duanlikang@didichuxing.com>
2021-05-20 18:02:05 +0800

    doc: update tenon demo of input and textarea

--- examples/tenon-vue/src/component-input/app.vue
+++ examples/tenon-vue/src/component-input/app.vue
@@ -1,61 +1,86 @@
 <template>
-  <view>
-    <view>
-      <text class="demo-title">Component Input!</text>
+  <view class="page">
+    <view class="demo-header">
+      <text class="demo-title">Component Input</text>
     </view>
-    <view class="demo-item">
-      <text class="demo-item-title">Input</text>
-      <view class="demo-box">
-        <input class="input"/>
+    <view class="demo-container">
+      <view class="demo-item">
+        <view class="item-title">
+          <text class="item-title-text">Input</text>
+        </view>
+        <view class="item-container">
+          <input class="input" />
+        </view>
       </view>
-    </view>
-    <view class="demo-item">
-      <text class="demo-item-title">Input 含有placeholder</text>
-      <view class="demo-box">
-         <input 
-          class="input input-place" 
-          placeholder="我是PlaceHolder" 
+      <view class="demo-item">
+        <view class="item-title">
+          <text class="item-title-text">Input 含有placeholder</text>
+        </view>
+        <view class="item-container">
+          <input class="input input-place" placeholder="我是PlaceHolder" />
+        </view>
+      </view>
+      <view class="demo-item">
+        <view class="item-title">
+          <text class="item-title-text">Input(focused)</text>
+        </view>
+        <view class="item-container">
+          <input class="input input-place" :focused="focused" />
+        </view>
+        <view class="operation-container">
+          <button class="btn" @tap="toggleFocus">Change Input Focus</button>
+        </view>
+      </view>
+      <view class="demo-item">
+        <view class="item-title">
+          <text class="item-title-text">Input(MaxLength)</text>
+        </view>
+        <view class="item-container">
+          <input class="input" :maxLength="maxLength" />
+        </view>
+      </view>
+      <view class="demo-item">
+        <view class="item-title">
+          <text class="item-title-text">Input(type)</text>
+          <text class="item-title-text">type: {{ type }}</text>
+        </view>
+        <view class="item-container">
+          <input class="input" :type="type" />
+        </view>
+        <view class="operation-container">
+          <button class="btn" @tap="handleChangeType">Change Input Type</button>
+        </view>
+      </view>
+      <view class="demo-item">
+        <view class="item-title">
+          <text class="item-title-text">Input(returnKeyType)</text>
+          <text class="item-title-text"
+            >returnKeyType: {{ returnKeyType }}</text
+          >
+        </view>
+        <view class="item-container">
+          <input class="input" :returnKeyType="returnKeyType" />
+        </view>
+        <view class="operation-container">
+          <button class="btn" @tap="handleChangeKeyType">
+            Change ReturnKey Type
+          </button>
+        </view>
+      </view>
+      <view class="demo-item">
+        <view class="item-title">
+          <text class="item-title-text">Input事件</text>
+        </view>
+        <view class="item-container">
+          <input
+            class="input"
+            @input="handleInput"
+            @change="handleChange"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @confirm="handleConfirm"
           />
-      </view>
-    </view>
-    <view class="demo-item">
-      <text class="demo-item-title">Input设定focused</text>
-      <view class="demo-box">
-        <input class="input" :focused="focused"/>
-        <text class="btn" @tap="toggleFocus">Toggle Focus</text>
-      </view>
-    </view>
-    <view class="demo-item">
-      <text class="demo-item-title">Input设定max</text>
-      <view class="demo-box">
-        <input class="input" :max-length="maxLength"/>
-      </view>
-    </view>
-    <view class="demo-item">
-      <text class="demo-item-title">Input设定type</text>
-      <view class="demo-box">
-        <text>当前Type:{{type}}</text>
-        <input class="input" :type="type"/>
-        <button @tap="handleChangeType">Change Input Type</button>
-      </view>
-    </view>
-     <view class="demo-item">
-      <text class="demo-item-title">Input设定ReturnKeyType</text>
-      <view class="demo-box">
-        <text>当前Return Key:{{returnKeyType}}</text>
-        <input class="input" :return-key-type="returnKeyType"/>
-        <button @tap="handleChangeKeyType">Change ReturnKey Type</button>
-      </view>
-    </view>
-    <view class="demo-item">
-      <text class="demo-item-title">Input事件测试</text>
-      <view class="demo-box">
-        <input class="input" 
-          @input="handleInput" 
-          @change="handleChange"
-          @focus="handleFocus"
-          @blur="handleBlur"
-          @confirm="handleConfirm"/>
+        </view>
       </view>
     </view>
   </view>
@@ -64,62 +89,59 @@
 
 <script>
 let i = 0;
-let types = ['default','number','tel','email','password']
-let returnTypes = ['done' ,'go' ,'next','search' ,'send']
+let types = ["default", "number", "tel", "email", "password"];
+let returnTypes = ["done", "go", "next", "search", "send"];
 export default {
-  data(){
+  data() {
     return {
       focused: false,
       maxLength: 10,
-      type: 'email',
-      returnKeyType: 'done'
-    }
+      type: "email",
+      returnKeyType: "done",
+    };
   },
   methods: {
-    toggleFocus(){
-      this.focused = !this.focused
+    toggleFocus() {
+      this.focused = !this.focused;
     },
-    handleChangeType(){
-      this.type = types[i++%5]
+    handleChangeType() {
+      this.type = types[i++ % 5];
     },
-    handleChangeKeyType(){
-      this.returnKeyType = returnTypes[i++%5]
+    handleChangeKeyType() {
+      this.returnKeyType = returnTypes[i++ % 5];
     },
-    handleInput({value,text, state}){
-      console.log('Input Event:', value,text, state)
+    handleInput({ value, text, state }) {
+      console.log("Input Event:", value, text, state);
     },
-    handleChange(value){
-      console.log('Change Event:', value)
+    handleChange(value) {
+      console.log("Change Event:", value);
     },
-    handleFocus(value){
-      console.log('Focus Event:', value)
+    handleFocus() {
+      console.log("Focus Event:");
     },
-     handleBlur(value){
-      console.log('Blur Event:', value)
+    handleBlur(value) {
+      console.log("Blur Event:");
     },
-     handleConfirm(value){
-      console.log('Confirm Event:', value)
-    }
-  }
-}
+    handleConfirm(value) {
+      console.log("Confirm Event:", value);
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-  .demo-title{
-    font-size: .36rem;
-    width: 100%;
-    text-align: center;
-  }
-  .input{
-    width: 100%;
-    background-color: aquamarine;
-    height: .5rem;
-  }
-  .input-place{
-   placeholder-color: blue;
-   cursor-color: #eedeee;
-   placeholder-font-size: .24rem;
-  }
+@import "../common/assets/css/common.less";
+
+.input {
+  width: 100%;
+  background-color: #eeffee;
+  height: 0.5rem;
+}
+.input-place {
+  placeholder-color: blue;
+  cursor-color: #eedeee;
+  placeholder-font-size: 0.24rem;
+}
 .btn {
   flex: 1;
   margin: 0 0.2rem;
@@ -129,7 +151,7 @@ export default {
   background: #fa9153;
   border-radius: 10px;
 }
-  .demo-box{
-    margin-top: .2rem;
-  }
+.demo-box {
+  margin-top: 0.2rem;
+}
 </style>
