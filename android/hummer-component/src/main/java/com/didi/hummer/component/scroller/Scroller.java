7a54871266b7c14d8795d4fd524fb9f2be7ab1d8
XiaoFeng <bbssyyuui@qq.com>
2021-06-02 18:24:02 +0800

    fix(Android): 修复Scroller组件和HorizontalScroller设置min、max宽度或高度后无法滚动的问题

--- android/hummer-component/src/main/java/com/didi/hummer/component/scroller/Scroller.java
+++ android/hummer-component/src/main/java/com/didi/hummer/component/scroller/Scroller.java
@@ -137,6 +137,7 @@ public class Scroller extends HMBase<SmartRefreshLayout> implements HMBase.Posit
         // 把最外层的Yoga属性复制一份给最内层的Layout，使JS侧设置的样式生效
         layout.getYogaNode().copyStyle(getYogaNode());
         adjustWidthAndHeight();
+        adjustMinMaxWidthAndHeight();
     }
 
     private void initScrollView() {
@@ -234,6 +235,13 @@ public class Scroller extends HMBase<SmartRefreshLayout> implements HMBase.Posit
         } else {
             layout.getYogaNode().setHeightPercent(100);
         }
+    }
+
+    private void adjustMinMaxWidthAndHeight() {
+        layout.getYogaNode().setMinWidth(Float.NaN);
+        layout.getYogaNode().setMaxWidth(Float.NaN);
+        layout.getYogaNode().setMinHeight(Float.NaN);
+        layout.getYogaNode().setMaxHeight(Float.NaN);
     }
 
     @JsMethod("appendChild")
