d6735bd16efef5f3ce9a48806a7f1256ee5bb80f
duanlikang <duanlikang@didichuxing.com>
2021-06-04 11:52:08 +0800

    chore: remove dist file

--- examples/tenon-react/src/index/entry.js
+++ examples/tenon-react/src/index/entry.js
@@ -2,10 +2,6 @@ import React from "react"
 import App from "./App"
 import * as  Tenon from "@hummer/tenon-react"
 
-<<<<<<< HEAD
 import "./App.css"
-=======
-// import "./App.css"
->>>>>>> 18c60762 (feat: develop react)
 
 Tenon.render(<App />)
