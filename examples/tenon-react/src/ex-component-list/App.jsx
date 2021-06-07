c07efe8127160e7230dccf99312351b50406ae47
duanlikang <duanlikang@didichuxing.com>
2021-06-07 14:21:12 +0800

    doc: add tenon react ex-component demo

---
+++ examples/tenon-react/src/ex-component-list/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PageItem from '@common/components/PageItem';
+import DemoItem from '@common/components/DemoItem';
+
+function App() {
+  return (
+    <PageItem title="Component List">
+      <DemoItem title="CanScroll">
+        <text>Component List</text>
+      </DemoItem>
+    </PageItem>
+  );
+}
+
+export default App;
