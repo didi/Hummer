5b4fceebe3a7865a6c129d843256753b06e45419
duanlikang <duanlikang@didichuxing.com>
2021-06-10 14:40:18 +0800

    feat: fix react event 重复添加的问题

--- tenon/packages/tenon-react/src/events/listener.ts
+++ tenon/packages/tenon-react/src/events/listener.ts
@@ -1,7 +1,12 @@
+import {Base as Element} from "@hummer/tenon-core"
+import {getFiberCurrentPropsFromNode} from '../hostConfig/utils'
 /**
  * 获取标准的 Listener函数
  */
-export function getListener(func:Function){
-  // TODO 封装标准的 Listener 函数，进行返回值的统一封装
-  return func
+export function getListener(node: Element, propName: string, func:Function){
+  return function(...args: Array<any>){
+    let props = getFiberCurrentPropsFromNode(node);
+    let originFunc = props[propName];
+    originFunc.apply(null, args)
+  }
 }
