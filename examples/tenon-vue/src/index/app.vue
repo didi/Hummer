290c79b840a6cd10b32e550359a25520578a89cf
duanlikang <577218983@qq.com>
2021-06-22 16:10:04 +0800

    docs: update tenon vue demo to remove vw

--- examples/tenon-vue/src/index/app.vue
+++ examples/tenon-vue/src/index/app.vue
@@ -5,8 +5,8 @@
 </template>
 <style lang="less" scoped>
 .page{
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
