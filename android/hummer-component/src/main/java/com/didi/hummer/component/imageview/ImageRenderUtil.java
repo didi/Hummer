2f1da00b60e054a9440a52e0789b757379ef2b7a
liuliwei <liuliwei@didiglobal.com>
2021-06-23 16:51:25 +0800

    feat(image):修改加载失败时 srcType对应值

--- android/hummer-component/src/main/java/com/didi/hummer/component/imageview/ImageRenderUtil.java
+++ android/hummer-component/src/main/java/com/didi/hummer/component/imageview/ImageRenderUtil.java
@@ -156,9 +156,9 @@ public class ImageRenderUtil {
             byte[] bitmapArray = Base64.decode(imageSrc.split(",")[1], Base64.DEFAULT);
             Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.length);
             imageView.setImageBitmap(bitmap);
-            completeCallback.call(2, true);
+            completeCallback.call(LOCAL_SRC, true);
         } catch (Exception e) {
-            completeCallback.call(-1, false);
+            completeCallback.call(FAIL_SRC, false);
         }
     }
 
