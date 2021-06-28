f0881ad73cc5790aed49b645f74baa22c3ea558b
duanlikang <577218983@qq.com>
2021-06-28 19:42:43 +0800

    fix(tenon): 修复 Tenon Border Color Rgba形式识别失败的问题

--- tenon/packages/tenon-utils/src/style/common/utils.ts
+++ tenon/packages/tenon-utils/src/style/common/utils.ts
@@ -2,7 +2,9 @@ export type Length = string | number
 export type UnitWidth = string
 export type Width =  Length | Keyword.AUTO
 export type Style = Record<string, string | number>
+export type RgbaColor = string
 
+export const rgbaReg = /rgba?\([\d\.\s,]+\)/
 
 export enum Keyword {
   AUTO = "auto"
@@ -14,7 +16,6 @@ export enum BorderStyle {
   DASHED = 'dashed',
   DOTTED = 'dotted'
 }
-
 
 /**
  * 判断是否是 Number 类型
@@ -56,4 +57,26 @@ export function isLength(length: any):Boolean{
   return [BorderStyle.NONE,BorderStyle.DASHED,BorderStyle.DOTTED, BorderStyle.SOLID].findIndex((borderStyle:BorderStyle) => {
     return value === borderStyle
   }) !== -1
+}
+
+/**
+ * 是否包含 Rgba Color
+ * @param value 
+ * @returns 
+ */
+export function hasRgbaColor(value:string):Boolean{
+  return rgbaReg.test(value)
+}
+
+export function getRgbaColor(value:string):RgbaColor|null{
+  if(!value){
+    return null
+  }
+  let matches = value.match(rgbaReg)
+  if(matches && matches.length > 0){
+    let matchStr = matches[0]
+    return matchStr
+  }else{
+    return null
+  }
 }
--- tenon/packages/tenon-utils/src/style/transformer/border.ts
+++ tenon/packages/tenon-utils/src/style/transformer/border.ts
@@ -1,4 +1,4 @@
-import {isBorderStyle, isLength, Style} from '../common/utils'
+import {isBorderStyle, isLength, Style, hasRgbaColor, getRgbaColor} from '../common/utils'
 import {extend} from '../../index'
 const attrs = ["border","border-left", "border-right", "border-top", "border-bottom"]
 
@@ -22,22 +22,48 @@ export function transformBorder(style:Record<string, string>):Record<string, str
  * 参考 MDN 文档：https://developer.mozilla.org/en-US/docs/Web/CSS/border
  * The border property may be specified using one, two, or three of the values listed below. The order of the values does not matter.
  * @param attr 待拆解的属性
- * @param value 待拆解的属性值
+ * @param value 待拆解的属性值 * 
  * ex.borderLeft: 1px solid #eee; borderWidth: 1px; borderStyle: solid; borderColor: #eee;
  * ex.border: none; borderStyle: none;
+ * ex.border: 1px 
  */
 function transformBorderStyle(attr:string, borderValue:string){
-  let values = borderValue.trim().split(/\s+/);
   let tempStyle:Style = {};
-  for(let i=0,len=values.length; i<len; i++){
-    if(isBorderStyle(values[i])){
-      tempStyle[attr + "-style"] = values[i]
-    }else if(isLength(values[i])){
-      tempStyle[attr + "-width"] = values[i]
+  // 处理 rgba 的颜色场景
+  if(!hasRgbaColor(borderValue)){
+    let values = borderValue.trim().split(/\s+/);
+    tempStyle = traverseBorderValue(values, attr)
+  }else{
+    let rgbaColor = getRgbaColor(borderValue)
+    if(rgbaColor){
+      borderValue = borderValue.split(rgbaColor).join("")
+      let values = borderValue.trim().split(/\s+/)
+      tempStyle = traverseBorderValue(values, attr)
+      tempStyle[attr +'-color'] = rgbaColor
     }else{
-      tempStyle[attr + "-color"] = values[i]
+      let values = borderValue.trim().split(/\s+/);
+      tempStyle = traverseBorderValue(values, attr)
     }
   }
   return tempStyle
 }
 
+/**
+ * 遍历数组，获取标准的 Border Value
+ * @param values Array<string>
+ * @param attr Unit Border Type
+ * @returns Style
+ */
+function traverseBorderValue(values:Array<string>, attr:string){
+  let tempStyle:Style = {}
+  values.forEach((value) => {
+    if(isBorderStyle(value)){
+      tempStyle[attr + "-style"] = value
+    }else if(isLength(value)){
+      tempStyle[attr + "-width"] = value
+    }else{
+      tempStyle[attr + "-color"] = value
+    }
+  })
+  return tempStyle
+}
