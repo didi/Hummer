631a1e677c0c07a0ffa1500af98c30bced439d19
duanlikang <577218983@qq.com>
2021-07-13 20:47:32 +0800

    docs(tenon): Tenon React 新增可拦截视图的原生组件拓展

--- examples/tenon-react/src/ex-component-test/App.jsx
+++ examples/tenon-react/src/ex-component-test/App.jsx
@@ -5,6 +5,8 @@ import DemoItem from '@common/components/DemoItem';
 
 function App() {
   const [eventValue, setEventValue] = useState("Event View")
+  const [safeValue, setSafeValue] = useState("Card Content: SafeValue")
+
   return (
     <PageItem title="原生组件拓展">
       <DemoItem title="纯视图组件拓展">
@@ -16,6 +18,18 @@ function App() {
       <DemoItem title="纯视图组件（属性&方法）">
         <ex-event-view className="event-view" value={eventValue} onChange={(value) => {console.log(`Value Change:${value}`)}}></ex-event-view>
         <button className="btn" onTap={() => setEventValue(`Random Value:${Math.floor(Math.random() * 1000)}`)}>Change Event Value</button>
+      </DemoItem>
+      <DemoItem title="拦截子视图组件">
+        <view className="view-container">
+          <ex-safe-view className="safe-view" headerText="Card Header">
+            <view className="safe-view-content">
+              <text>{safeValue}</text>
+            </view>
+            <button className="btn" onTap={() => setSafeValue(`Card Content: ${Math.floor(Math.random() * 1000)}`)}>
+              Update SafeValue
+            </button>
+          </ex-safe-view>
+        </view>
       </DemoItem>
     </PageItem>
   );
--- examples/tenon-react/src/ex-component-test/entry.js
+++ examples/tenon-react/src/ex-component-test/entry.js
@@ -7,9 +7,11 @@ import "./App.less"
 import SimpleView from './native-components/simple-view'
 import AttrView from './native-components/attr-view'
 import EventView from './native-components/event-view'
+import SafeView from './native-components/safe-view'
+
 
 Tenon.register(AttrView)
 Tenon.register(SimpleView)
 Tenon.register(EventView)
-
+Tenon.register(SafeView)
 Tenon.render(<App />)
