7a32d93ba691fa7ba0b90df15ed61f47301f0bc0
firelion0705 <firelion0725@163.com>
2021-07-29 17:01:39 +0800

    android:feat: 修正api 及demo

--- android/hummer-component/src/main/java/com/didi/hummer/component/canvas/CanvasDrawHelperView.java
+++ android/hummer-component/src/main/java/com/didi/hummer/component/canvas/CanvasDrawHelperView.java
@@ -15,6 +15,9 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import com.didi.hummer.render.style.HummerStyleUtils;
+import com.didi.hummer.render.utility.DPUtil;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -58,6 +61,10 @@ public class CanvasDrawHelperView extends View {
         return canvasContext.getTextPaint();
     }
 
+    private float dp2px(Object v) {
+        return HummerStyleUtils.convertNumber(v);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -70,27 +77,35 @@ public class CanvasDrawHelperView extends View {
     /**
      * 绘制图片
      */
-    public void drawImage(Bitmap bitmap, float x, float y, float dWidth, float dHeight) {
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
-        Matrix matrix = new Matrix();
-        float scaleWidth = ((float) dWidth) / width;
-        float scaleHeight = ((float) dHeight) / height;
-        matrix.postScale(scaleWidth, scaleHeight);
-        Bitmap newBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
-        actions.add(canvas -> canvas.drawBitmap(newBitmap, x, y, getLinePaint()));
+    public void drawImage(Bitmap bitmap, Object x, Object y, Object dWidth, Object dHeight) {
+        actions.add(canvas -> {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            Matrix matrix = new Matrix();
+            float scaleWidth = ((float) dWidth) / width;
+            float scaleHeight = ((float) dHeight) / height;
+            matrix.postScale(scaleWidth, scaleHeight);
+            Bitmap newBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+            float x_px = dp2px(x);
+            float y_px = dp2px(y);
+            canvas.drawBitmap(newBitmap, x_px, y_px, getLinePaint());
+        });
         invalidate();
     }
 
     /**
      * 绘制矩形 实心
      */
-    public void fillRect(float x, float y, float width, float height) {
+    public void fillRect(Object x, Object y, Object width, Object height) {
         Log.i(TAG, "fillRect do");
-        getLinePaint().setStyle(Paint.Style.FILL);
         actions.add(canvas -> {
+            getFillPaint().setStyle(Paint.Style.FILL);
             Log.i(TAG, "fillRect Action");
-            Rect rect = new Rect((int) x, (int) y, (int) (x + width), (int) (y + height));
+            float x_px = dp2px(x);
+            float y_px = dp2px(y);
+            float width_px = dp2px(width);
+            float height_px = dp2px(height);
+            Rect rect = new Rect((int) x_px, (int) y_px, (int) (x_px + width_px), (int) (y_px + height_px));
             canvas.drawRect(rect, getFillPaint());
         });
         invalidate();
@@ -99,10 +114,14 @@ public class CanvasDrawHelperView extends View {
     /**
      * 绘制矩形 镂空
      */
-    public void strokeRect(float x, float y, float width, float height) {
-        getLinePaint().setStyle(Paint.Style.STROKE);
+    public void strokeRect(Object x, Object y, Object width, Object height) {
         actions.add(canvas -> {
-            Rect rect = new Rect((int) x, (int) y, (int) (x + width), (int) (y + height));
+            getLinePaint().setStyle(Paint.Style.STROKE);
+            float x_px = dp2px(x);
+            float y_px = dp2px(y);
+            float width_px = dp2px(width);
+            float height_px = dp2px(height);
+            Rect rect = new Rect((int) x_px, (int) y_px, (int) (x_px + width_px), (int) (y_px + height_px));
             canvas.drawRect(rect, getLinePaint());
         });
         invalidate();
@@ -111,72 +130,119 @@ public class CanvasDrawHelperView extends View {
     /**
      * 绘制圆形 实心
      */
-    public void fillCircle(float x, float y, float radius) {
-        getLinePaint().setStyle(Paint.Style.FILL);
-        actions.add(canvas -> canvas.drawCircle(x, y, radius, getFillPaint()));
+    public void fillCircle(Object x, Object y, Object radius) {
+        actions.add(canvas -> {
+            getFillPaint().setStyle(Paint.Style.FILL);
+            float x_px = dp2px(x);
+            float y_px = dp2px(y);
+            float radius_px = dp2px(radius);
+            canvas.drawCircle(x_px, y_px, radius_px, getFillPaint());
+        });
         invalidate();
     }
 
     /**
      * 绘制圆形 镂空
      */
-    public void strokeCircle(float x, float y, float radius) {
-        getLinePaint().setStyle(Paint.Style.STROKE);
-        actions.add(canvas -> canvas.drawCircle(x, y, radius, getLinePaint()));
+    public void strokeCircle(Object x, Object y, Object radius) {
+        actions.add(canvas -> {
+            getLinePaint().setStyle(Paint.Style.STROKE);
+            float x_px = dp2px(x);
+            float y_px = dp2px(y);
+            float radius_px = dp2px(radius);
+            canvas.drawCircle(x_px, y_px, radius_px, getLinePaint());
+        });
         invalidate();
     }
 
     public void fontSize(float size) {
-        getTextPaint().setTextSize(size);
+        actions.add(canvas -> {
+            float size_px = dp2px(size);
+            getTextPaint().setTextSize(size_px);
+        });
     }
 
     /**
      * Text 绘制改为左上角起始绘制 该方式和IOS 保持一致 且符合StaticLayout 绘制能力
      */
-    public void fillText(String text, float x, float y, int maxWidth) {
+    public void fillText(String text, Object x, Object y, Object maxWidth) {
         actions.add(canvas -> {
-            StaticLayout staticLayout = new StaticLayout(text, getTextPaint(), maxWidth, Alignment.ALIGN_NORMAL, 1, 0, true);
+            float x_px = dp2px(x);
+            float y_px = dp2px(y);
+            float width_px = dp2px(maxWidth);
+            StaticLayout staticLayout = new StaticLayout(text, getTextPaint(), (int) width_px, Alignment.ALIGN_NORMAL, 1, 0, true);
             canvas.save();
-            canvas.translate(x, y);
+            canvas.translate(x_px, y_px);
             staticLayout.draw(canvas);
             canvas.restore();
         });
         invalidate();
     }
 
-    public void arc(float x, float y, float radius, float startAngle, float endAngle, int clockwise) {
+    public void arc(Object x, Object y, Object radius, Object startAngle, Object endAngle, Object clockwise) {
         actions.add(canvas -> {
-            RectF rectF = new RectF(x - radius, y - radius, x + radius, y + radius);
-            canvas.drawArc(rectF, startAngle, endAngle, false, getLinePaint());
+            float x_px = dp2px(x);
+            float y_px = dp2px(y);
+            float radius_px = dp2px(radius);
+            float startAngle_px = dp2px(startAngle);
+            float endAngle_px = dp2px(endAngle);
+            RectF rectF = new RectF(x_px - radius_px, y_px - radius_px, x_px + radius_px, y_px + radius_px);
+            canvas.drawArc(rectF, startAngle_px, endAngle_px, false, getLinePaint());
         });
     }
 
-    public void drawLine(float startX, float startY, float stopX, float stopY) {
+    public void drawLine(Object startX, Object startY, Object stopX, Object stopY) {
         Log.i(TAG, "drawLine do");
         actions.add(canvas -> {
+            float x_start_px = dp2px(startX);
+            float y_start_px = dp2px(startY);
+            float x_stop_px = dp2px(stopX);
+            float y_stop_px = dp2px(stopY);
             Log.i(TAG, "drawLine Action");
-            canvas.drawLine(startX, startY, stopX, stopY, getLinePaint());
+            canvas.drawLine(x_start_px, y_start_px, x_stop_px, y_stop_px, getLinePaint());
         });
         invalidate();
     }
 
-    public void drawLines(float[] points) {
+    public void drawLines(Object[] points) {
         actions.add(canvas -> {
+            float[] points_px = new float[points.length];
+            for (int i = 0; i < points.length; i++) {
+                points_px[i] = dp2px(points[i]);
+            }
             Log.i(TAG, "drawLines Action");
-            canvas.drawLines(points, getLinePaint());
+            canvas.drawLines(points_px, getLinePaint());
         });
         invalidate();
     }
 
     public void drawPath(CanvasPath path) {
-        actions.add(canvas -> canvas.drawPath(path.getPath(), getLinePaint()));
+        actions.add(canvas -> {
+            canvas.drawPath(path.getPath(), getLinePaint());
+        });
         invalidate();
     }
 
-    public void ellipse(float left, float top, float right, float bottom) {
+    public void strokeEllipse(Object left, Object top, Object right, Object bottom) {
         actions.add(canvas -> {
-            RectF rectF = new RectF(left, top, right, bottom);
+            float left_px = dp2px(left);
+            float top_px = dp2px(top);
+            float right_px = dp2px(right);
+            float bottom_px = dp2px(bottom);
+            RectF rectF = new RectF(left_px, top_px, right_px, bottom_px);
             canvas.drawOval(rectF, getLinePaint());
+        });
+        invalidate();
+    }
+
+    public void fillEllipse(Object left, Object top, Object right, Object bottom) {
+        actions.add(canvas -> {
+            float left_px = dp2px(left);
+            float top_px = dp2px(top);
+            float right_px = dp2px(right);
+            float bottom_px = dp2px(bottom);
+            RectF rectF = new RectF(left_px, top_px, right_px, bottom_px);
+            canvas.drawOval(rectF, getFillPaint());
         });
         invalidate();
     }
@@ -217,4 +283,11 @@ public class CanvasDrawHelperView extends View {
             canvasContext.fillColor(color);
         });
     }
+
+    public void textColor(String color) {
+        actions.add(canvas -> {
+            canvasContext.textColor(color);
+        });
+    }
+
 }
--- android/hummer-component/src/main/java/com/didi/hummer/component/canvas/CanvasPath.java
+++ android/hummer-component/src/main/java/com/didi/hummer/component/canvas/CanvasPath.java
@@ -1,27 +1,56 @@
 package com.didi.hummer.component.canvas;
 
+import android.content.Context;
 import android.graphics.Path;
+import android.util.Log;
+import android.view.View;
 
 import com.didi.hummer.annotation.Component;
 import com.didi.hummer.annotation.JsMethod;
+import com.didi.hummer.context.HummerContext;
+import com.didi.hummer.core.engine.JSValue;
+import com.didi.hummer.render.component.view.HMBase;
+import com.didi.hummer.render.style.HummerStyleUtils;
+import com.didi.hummer.render.utility.DPUtil;
 
 @Component("CanvasPath")
-public class CanvasPath {
+public class CanvasPath extends HMBase {
 
+    private Context context;
     private Path path = new Path();
+
+    public CanvasPath(HummerContext context, JSValue jsValue, String viewID) {
+        super(context, jsValue, viewID);
+        this.context = context;
+    }
 
     public Path getPath() {
         return path;
     }
 
+    private float dp2px(Object v) {
+        return HummerStyleUtils.convertNumber(v);
+    }
+
     @JsMethod("moveTo")
-    public void moveTo(float x, float y) {
-        path.moveTo(x, y);
+    public void moveTo(Object x, Object y) {
+        try {
+            Log.i("CanvasPath22222","x:"+x);
+            Log.i("CanvasPath22222","y:"+y);
+            float x_px = dp2px(x);
+            float y_px = dp2px(y);
+            path.moveTo(x_px, y_px);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @JsMethod("lineTo")
-    public void lineTo(float x, float y) {
-        path.lineTo(x, y);
+    public void lineTo(Object x, Object y) {
+        float x_px = dp2px(x);
+        float y_px = dp2px(y);
+        path.lineTo(x_px, y_px);
     }
 
     @JsMethod("close")
@@ -29,4 +58,8 @@ public class CanvasPath {
         path.close();
     }
 
+    @Override
+    protected View createViewInstance(Context context) {
+        return null;
+    }
 }
