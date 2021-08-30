316b79922ee7e939eaf9f8b64ecb998ffc6bc04f
jayconscious <zhuzhiyong_wade@163.com>
2021-08-30 19:29:53 +0800

    feat: development环境更新视图树结构

--- tenon/packages/tenon-vue/src/runtime/index.ts
+++ tenon/packages/tenon-vue/src/runtime/index.ts
@@ -70,6 +70,68 @@ export function render(App:any){
   let instance = app.mount(container);
   initPageLifeCycle(container, instance, App)
   container.render();
+  // console.log(container)
+
+  const NODE_VIEW = "Symbol(NODE_VIEW)";
+  const NODE_TEXT = "Symbol(NODE_TEXT)";
+  const NODE_IMAGE = "Symbol(NODE_IMAGE)"
+  const NODE_BUTTON = "Symbol(NODE_BUTTON)"
+  const NODE_TEXTAREA = "Symbol(NODE_TEXTAREA)"
+  const NODE_INPUT = "Symbol(NODE_INPUT)"
+  const NODE_SWITCH = "Symbol(NODE_SWITCH)"
+  const NODE_SCROLLER = "Symbol(NODE_SCROLLER)"
+  const NODE_REFRESH = "Symbol(NODE_REFRESH)"
+  const NODE_LOADMORE = "Symbol(NODE_LOADMORE)"
+  const ELEMNT_TAG_MAP = getElementTagMap();
+  function getElementTagMap() {
+    let tagMap = new Map();
+    tagMap.set(NODE_VIEW, 'view');
+    tagMap.set(NODE_TEXT, 'text');
+    tagMap.set(NODE_IMAGE, 'image');
+    tagMap.set(NODE_BUTTON, 'button');
+    tagMap.set(NODE_TEXTAREA, 'textarea');
+    tagMap.set(NODE_INPUT, 'input');
+    tagMap.set(NODE_SWITCH, 'switch');
+    tagMap.set(NODE_SCROLLER, 'scroller');
+    tagMap.set(NODE_REFRESH, 'refresh');
+    tagMap.set(NODE_LOADMORE, 'loadmore');
+    return tagMap;
+  }
+  function getViewData (container: any) {
+    let simpleRoot = Object.create({})
+    let viewMap = Object.create({})
+    const treeTraveler = function(node: any, rootView: any) {
+      processView(node, rootView, viewMap)
+      if (node.children && node.children.size > 0) {
+        rootView.children = Array.apply(null, new Array(node.children.size)).map( () => {return {}})
+        let arr = Array.from(node.children)
+        arr.forEach((n, i) => {
+          treeTraveler(n, rootView.children[i])
+        })
+      }
+    }
+    treeTraveler(container, simpleRoot)
+    return {
+      simpleRoot,
+      viewMap
+    }
+  }
+  const processView = function(node: any, rootView: any, viewMap: any) {
+    rootView.__view_id = node.__view_id
+    rootView.name = node.__NAME && ELEMNT_TAG_MAP.get(node.__NAME.toString()) || 'template'
+    switch (rootView.name) {
+      case 'text':
+        rootView.text = node._text
+        break;
+      case 'image':
+        rootView.src = node._src
+        break;
+      default:
+        break;
+    }
+    viewMap[rootView.__view_id] = node
+  }
+  getViewData(container)
 
   if(NODE_DEBUG_ENV){
     // dev环境开启devtool
