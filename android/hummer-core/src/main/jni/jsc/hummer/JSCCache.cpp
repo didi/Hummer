c815795fe9948212e486cca3ab9121b5537693cb
XiaoFeng <bbssyyuui@qq.com>
2021-09-09 13:02:00 +0800

    chore(Android): JSContext保存和删除加锁，支持多线程创建和销毁

--- android/hummer-core/src/main/jni/jsc/hummer/JSCCache.cpp
+++ android/hummer-core/src/main/jni/jsc/hummer/JSCCache.cpp
@@ -2,20 +2,25 @@
 // Created by XiaoFeng on 2021/1/24.
 //
 
+#include <mutex>
 #include "JSCCache.h"
 
+std::mutex mtx;
 
 std::list<JSContextRef> JSCCache::jsContextRefList;
 
 void JSCCache::addJsContextRef(JSContextRef jsContextRef) {
+    std::lock_guard<std::mutex> locker(mtx);
     jsContextRefList.push_back(jsContextRef);
 }
 
 void JSCCache::removeJsContextRef(JSContextRef jsContextRef) {
+    std::lock_guard<std::mutex> locker(mtx);
     jsContextRefList.remove(jsContextRef);
 }
 
 bool JSCCache::findJSContextRef(JSContextRef jsContextRef) {
+    std::lock_guard<std::mutex> locker(mtx);
     auto it = std::find(jsContextRefList.begin(), jsContextRefList.end(), jsContextRef);
     return it != jsContextRefList.end();
 }
