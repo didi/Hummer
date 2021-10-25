bd8e3b1d48035eb3d65a105e5fb21395197b5347
ChasonTang <chasontang@warmbloom.com>
2021-10-25 16:12:22 +0800

    chore(iOS): Complete compatible function

--- iOS/Hummer/Classes/Core/HMView.m
+++ iOS/Hummer/Classes/Core/HMView.m
@@ -6,8 +6,18 @@
 //
 
 #import "HMView.h"
+#import <Hummer/HMConverter.h>
+#import <Hummer/UIView+HMImageLoader.h>
+#import <Hummer/HMJSGlobal.h>
+#import <Hummer/HMAttrManager.h>
+#import <Hummer/HMYogaConfig.h>
+#import <Hummer/UIView+HMRenderObject.h>
+#import <Hummer/HMTransitionAnimation.h>
 #import <Hummer/HMBorderDrawing.h>
 #import <Hummer/UIView+HMDom.h>
+#import <Hummer/HMExportManager.h>
+#import <Hummer/UIView+HMAttribute.h>
+#import <Hummer/HMGradientColor.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -29,7 +39,17 @@ static void updateShadowPathForView(HMView *view);
     UIColor *_Nullable _backgroundColor;
 }
 
-//@property (nonatomic, nullable, copy) UIColor *hmBackgroundColor;
+@property(nonatomic, nullable, weak) CAGradientLayer *gradientLayer;
+
+@property(nonatomic, nullable, copy) NSDictionary<NSString *, id <NSCopying, NSSecureCoding>> *styleStore;
+
+@property(nonatomic, assign) BOOL isBoxSizing;
+
+@property(nonatomic, readonly) NSSet<NSString *> *flexStyleKeySet;
+
+@property(nonatomic, readonly) NSSet<NSString *> *transitionKeySet;
+
+@property(nonatomic, readonly) NSSet<NSString *> *borderStyleSet;
 
 - (void)commonInit;
 
@@ -82,7 +102,412 @@ void updateShadowPathForView(HMView *view) {
     }
 }
 
+// TODO(ChasonTang): Rename to View, rename View to OldView
+HM_EXPORT_CLASS(NewView, HMView)
+
 @implementation HMView
+
+- (NSSet<NSString *> *)transitionKeySet {
+    static NSSet<NSString *> *transitionSet;
+    static dispatch_once_t dispatchOnce;
+    dispatch_once(&dispatchOnce, ^{
+        transitionSet = [NSSet setWithArray:@[
+                @"transitionDelay",
+                @"transitionDuration",
+                @"transitionProperty",
+                @"transitionTimingFunction",
+        ]];
+    });
+
+    return transitionSet;
+}
+
+- (NSSet<NSString *> *)borderStyleSet {
+    static NSSet<NSString *> *borderStyleSet;
+    static dispatch_once_t dispatchOnce;
+
+    dispatch_once(&dispatchOnce, ^{
+        // alphabet order
+        borderStyleSet = [NSSet setWithArray:@[
+                @"borderBottomWidth",
+                @"borderEndWidth",
+                @"borderLeftWidth",
+                @"borderRightWidth",
+                @"borderStartWidth",
+                @"borderTopWidth",
+                @"borderWidth",
+        ]];
+    });
+
+    return borderStyleSet;
+}
+
+- (NSSet<NSString *> *)flexStyleKeySet {
+    static NSSet<NSString *> *flexStyleSet;
+    static dispatch_once_t dispatchOnce;
+    dispatch_once(&dispatchOnce, ^{
+        // alphabet order
+        flexStyleSet = [NSSet setWithArray:@[
+                @"alignContent",
+                @"alignItems",
+                @"alignSelf",
+                @"aspectRatio",
+                @"borderBottomWidth",
+                @"borderEndWidth",
+                @"borderLeftWidth",
+                @"borderRightWidth",
+                @"borderStartWidth",
+                @"borderTopWidth",
+                @"borderWidth",
+                @"bottom",
+                @"display",
+                @"end",
+                @"flex",
+                @"flexBasis",
+                @"flexDirection",
+                @"flexGrow",
+                @"flexShrink",
+                @"flexWrap",
+                @"height",
+                @"justifyContent",
+                @"left",
+                @"margin",
+                @"marginBottom",
+                @"marginEnd",
+                @"marginHorizontal",
+                @"marginLeft",
+                @"marginRight",
+                @"marginStart",
+                @"marginTop",
+                @"marginVertical",
+                @"position",
+                @"right",
+                @"start",
+                @"top",
+                @"width",
+//                @"zIndex",
+//                @"direction"
+        ]];
+    });
+
+    return flexStyleSet;
+}
+
+//HM_EXPORT_PROPERTY(style, hm_style, hm_setStyle:)
+//HM_EXPORT_ATTRIBUTE(backgroundColor, __backgroundColor, HMStringToColor:)
+//HM_EXPORT_ATTRIBUTE(backgroundImage, __backgroundImage, HMStringOrigin:)
+//HM_EXPORT_ATTRIBUTE(borderStyle, __borderStyle, HMStringToBorderStyleList:)
+//HM_EXPORT_ATTRIBUTE(borderLeftStyle, __borderLeftStyle, HMStringToBorderStyle:)
+//HM_EXPORT_ATTRIBUTE(borderTopStyle, __borderTopStyle, HMStringToBorderStyle:)
+//HM_EXPORT_ATTRIBUTE(borderRightStyle, __borderRightStyle, HMStringToBorderStyle:)
+//HM_EXPORT_ATTRIBUTE(borderBottomStyle, __borderBottomStyle, HMStringToBorderStyle:)
+//HM_EXPORT_ATTRIBUTE(borderColor, __borderColor, HMStringToBorderColorList:)
+//HM_EXPORT_ATTRIBUTE(borderLeftColor, __borderLeftColor, HMStringToColor:)
+//HM_EXPORT_ATTRIBUTE(borderTopColor, __borderTopColor, HMStringToColor:)
+//HM_EXPORT_ATTRIBUTE(borderRightColor, __borderRightColor, HMStringToColor:)
+//HM_EXPORT_ATTRIBUTE(borderBottomColor, __borderBottomColor, HMStringToColor:)
+//HM_EXPORT_ATTRIBUTE(borderWidth, __borderWidth, HMStringToNumberArray:)
+//HM_EXPORT_ATTRIBUTE(borderLeftWidth, __borderLeftWidth, HMStringToFloat:)
+//HM_EXPORT_ATTRIBUTE(borderTopWidth, __borderTopWidth, HMStringToFloat:)
+//HM_EXPORT_ATTRIBUTE(borderRightWidth, __borderRightWidth, HMStringToFloat:)
+//HM_EXPORT_ATTRIBUTE(borderBottomWidth, __borderBottomWidth, HMStringToFloat:)
+//HM_EXPORT_ATTRIBUTE(borderRadius, __borderRadius, HMStringToBorderRadiusList:)
+//HM_EXPORT_ATTRIBUTE(borderTopLeftRadius, __borderTopLeftRadius, HMNumberToYGPoint:)
+//HM_EXPORT_ATTRIBUTE(borderTopRightRadius, __borderTopRightRadius, HMNumberToYGPoint:)
+//HM_EXPORT_ATTRIBUTE(borderBottomLeftRadius, __borderBottomLeftRadius, HMNumberToYGPoint:)
+//HM_EXPORT_ATTRIBUTE(borderBottomRightRadius, __borderBottomRightRadius, HMNumberToYGPoint:)
+//HM_EXPORT_ATTRIBUTE(shadow, __shadow, HMStringToShadowAttributes:)
+
+- (void)set__backgroundImage:(nullable id)parameter {
+    NSParameterAssert([parameter isKindOfClass:NSString.class]);
+    NSString *imageString = parameter;
+    [self hm_internalSetImageWithURL:imageString inJSBundleSource:nil context:nil completion:^(UIImage *_Nullable image, NSData *_Nullable data, NSError *_Nullable error, HMImageCacheType cacheType) {
+        if (image) {
+            self.layer.contents = (__bridge id _Nullable) (image.CGImage);
+        }
+    }];
+}
+
+- (void)set__borderStyle:(nullable id)list {
+    NSParameterAssert([list isKindOfClass:NSArray.class]);
+    id borderStyleObject = ((NSArray *) list).lastObject;
+    if (![borderStyleObject isKindOfClass:NSNumber.class]) {
+        return;
+    }
+    self.borderStyle = (HMBorderStyle) ((NSNumber *) borderStyleObject).unsignedIntegerValue;
+}
+
+- (void)set__borderLeftStyle:(HMBorderStyle)style {
+    self.borderStyle = style;
+}
+
+- (void)set__borderRightStyle:(HMBorderStyle)style {
+    self.borderStyle = style;
+}
+
+- (void)set__borderTopStyle:(HMBorderStyle)style {
+    self.borderStyle = style;
+}
+
+- (void)set__borderBottomStyle:(HMBorderStyle)style {
+    self.borderStyle = style;
+}
+
+- (void)set__borderColor:(nullable id)list {
+    NSParameterAssert([list isKindOfClass:NSArray.class]);
+    NSArray *borderColorList = list;
+    if (borderColorList.count == 1) {
+        borderColorList = @[borderColorList[0], borderColorList[0], borderColorList[0], borderColorList[0]];
+    } else if (borderColorList.count == 2) {
+        borderColorList = @[borderColorList[0], borderColorList[1], borderColorList[0], borderColorList[1]];
+    } else if (borderColorList.count == 3) {
+        borderColorList = @[borderColorList[0], borderColorList[1], borderColorList[2], borderColorList[1]];
+    }
+    id colorObject = borderColorList[0];
+    self.borderTopColor = [colorObject isKindOfClass:UIColor.class] ? colorObject : nil;
+    colorObject = borderColorList[1];
+    self.borderRightColor = [colorObject isKindOfClass:UIColor.class] ? colorObject : nil;
+    colorObject = borderColorList[2];
+    self.borderBottomColor = [colorObject isKindOfClass:UIColor.class] ? colorObject : nil;
+    colorObject = borderColorList[3];
+    self.borderLeftColor = [colorObject isKindOfClass:UIColor.class] ? colorObject : nil;
+}
+
+- (void)set__borderLeftColor:(id)color {
+    NSParameterAssert([color isKindOfClass:UIColor.class]);
+    self.borderLeftColor = color;
+}
+
+- (void)set__borderRightColor:(id)color {
+    NSParameterAssert([color isKindOfClass:UIColor.class]);
+    self.borderRightColor = color;
+}
+
+- (void)set__borderTopColor:(id)color {
+    NSParameterAssert([color isKindOfClass:UIColor.class]);
+    self.borderTopColor = color;
+}
+
+- (void)set__borderBottomColor:(id)color {
+    NSParameterAssert([color isKindOfClass:UIColor.class]);
+    self.borderBottomColor = color;
+}
+
+- (void)set__borderWidth:(nullable id)list {
+    NSParameterAssert([list isKindOfClass:NSArray.class]);
+    NSArray *borderWidthList = list;
+    if (borderWidthList.count == 1) {
+        borderWidthList = @[borderWidthList[0], borderWidthList[0], borderWidthList[0], borderWidthList[0]];
+    } else if (borderWidthList.count == 2) {
+        borderWidthList = @[borderWidthList[0], borderWidthList[1], borderWidthList[0], borderWidthList[1]];
+    } else if (borderWidthList.count == 3) {
+        borderWidthList = @[borderWidthList[0], borderWidthList[1], borderWidthList[2], borderWidthList[1]];
+    }
+
+    id numberObject = borderWidthList[0];
+    self.borderTopWidth = [numberObject isKindOfClass:NSNumber.class] ? ((NSNumber *) numberObject).doubleValue : -1;
+    numberObject = borderWidthList[1];
+    self.borderRightWidth = [numberObject isKindOfClass:NSNumber.class] ? ((NSNumber *) numberObject).doubleValue : -1;
+    numberObject = borderWidthList[2];
+    self.borderBottomWidth = [numberObject isKindOfClass:NSNumber.class] ? ((NSNumber *) numberObject).doubleValue : -1;
+    numberObject = borderWidthList[3];
+    self.borderLeftWidth = [numberObject isKindOfClass:NSNumber.class] ? ((NSNumber *) numberObject).doubleValue : -1;
+}
+
+- (void)set__borderLeftWidth:(CGFloat)width {
+    self.borderLeftWidth = width;
+}
+
+- (void)set__borderTopWidth:(CGFloat)width {
+    self.borderTopWidth = width;
+}
+
+- (void)set__borderRightWidth:(CGFloat)width {
+    self.borderRightWidth = width;
+}
+
+- (void)set__borderBottomWidth:(CGFloat)width {
+    self.borderBottomWidth = width;
+}
+
+- (void)set__borderRadius:(nullable id)list {
+    NSParameterAssert([list isKindOfClass:NSArray.class]);
+    NSArray *borderRadiusList = list;
+    if (borderRadiusList.count == 1) {
+        borderRadiusList = @[borderRadiusList[0], borderRadiusList[0], borderRadiusList[0], borderRadiusList[0]];
+    }
+    if (borderRadiusList.count == 2) {
+        borderRadiusList = @[borderRadiusList[0], borderRadiusList[1], borderRadiusList[0], borderRadiusList[1]];
+    }
+    if (borderRadiusList.count == 3) {
+        borderRadiusList = @[borderRadiusList[0], borderRadiusList[1], borderRadiusList[2], borderRadiusList[1]];
+    }
+
+    NSValue *listValue = list[0];
+    self.borderTopLeftRadius = listValue.ygValue.unit == YOGA_TYPE_WRAPPER(YGUnitPoint) ? listValue.ygValue.value : -1;
+    listValue = list[1];
+    self.borderTopRightRadius = listValue.ygValue.unit == YOGA_TYPE_WRAPPER(YGUnitPoint) ? listValue.ygValue.value : -1;
+    listValue = list[2];
+    self.borderBottomRightRadius = listValue.ygValue.unit == YOGA_TYPE_WRAPPER(YGUnitPoint) ? listValue.ygValue.value : -1;
+    listValue = list[3];
+    self.borderBottomLeftRadius = listValue.ygValue.unit == YOGA_TYPE_WRAPPER(YGUnitPoint) ? listValue.ygValue.value : -1;
+}
+
+- (void)set__borderTopLeftRadius:(YOGA_TYPE_WRAPPER(YGValue))radius {
+    self.borderTopLeftRadius = radius.unit == YOGA_TYPE_WRAPPER(YGUnitPoint) ? radius.value : -1;
+}
+
+- (void)set__borderTopRightRadius:(YOGA_TYPE_WRAPPER(YGValue))radius {
+    self.borderTopRightRadius = radius.unit == YOGA_TYPE_WRAPPER(YGUnitPoint) ? radius.value : -1;
+}
+
+- (void)set__borderBottomLeftRadius:(YOGA_TYPE_WRAPPER(YGValue))radius {
+    self.borderBottomLeftRadius = radius.unit == YOGA_TYPE_WRAPPER(YGUnitPoint) ? radius.value : -1;
+}
+
+- (void)set__borderBottomRightRadius:(YOGA_TYPE_WRAPPER(YGValue))radius {
+    self.borderBottomRightRadius = radius.unit == YOGA_TYPE_WRAPPER(YGUnitPoint) ? radius.value : -1;
+}
+
+- (void)set__backgroundColor:(nullable id)backgroundColor {
+    if (!backgroundColor) {
+        self.backgroundColor = nil;
+        [self.gradientLayer removeFromSuperlayer];
+    }
+    NSParameterAssert([backgroundColor isKindOfClass:UIColor.class]);
+    if ([backgroundColor isKindOfClass:HMGradientColor.class]) {
+        self.backgroundColor = nil;
+        if (!self.gradientLayer) {
+            CAGradientLayer *gradientLayer = CAGradientLayer.layer;
+            [self.layer insertSublayer:gradientLayer atIndex:0];
+            self.gradientLayer = gradientLayer;
+        }
+        HMGradientColor *gradientColor = backgroundColor;
+        self.gradientLayer.colors = @[(id) gradientColor.beginColor.CGColor, (id) gradientColor.endColor.CGColor];
+        self.gradientLayer.startPoint = gradientColor.beginPoint;
+        self.gradientLayer.endPoint = gradientColor.endPoint;
+
+        self.gradientLayer.frame = self.bounds;
+    } else {
+        [self.gradientLayer removeFromSuperlayer];
+        self.backgroundColor = backgroundColor;
+    }
+}
+
+- (void)set__shadow:(nullable NSArray<NSObject *> *)shadowAttributes {
+    if (shadowAttributes.count != 4) {
+        return;
+    }
+    NSParameterAssert(shadowAttributes.count == 4);
+    CGFloat widthOffset = [shadowAttributes[0] isKindOfClass:NSNumber.class] ? ((NSNumber *) shadowAttributes[0]).doubleValue : 0;
+    CGFloat heightOffset = [shadowAttributes[1] isKindOfClass:NSNumber.class] ? ((NSNumber *) shadowAttributes[1]).doubleValue : 0;
+    CGFloat shadowRadius = [shadowAttributes[2] isKindOfClass:NSNumber.class] ? ((NSNumber *) shadowAttributes[2]).doubleValue : 0;
+    UIColor *color = [shadowAttributes[3] isKindOfClass:UIColor.class] ? (UIColor *) shadowAttributes[3] : nil;
+
+    self.layer.shadowOpacity = (float) CGColorGetAlpha(color.CGColor);
+    self.layer.shadowColor = CGColorCreateCopyWithAlpha(color.CGColor, 1);
+    self.layer.shadowRadius = shadowRadius;
+    self.layer.shadowOffset = CGSizeMake(widthOffset, heightOffset);
+    // TODO(ChasonTang): [self.layer setNeedsDisplay];
+}
+
+- (NSDictionary<NSString *, NSObject *> *)hm_style {
+#ifndef NDEBUG
+    return self.styleStore;
+#else
+    return nil;
+#endif
+}
+
+- (void)hm_setStyle:(HMBaseValue *)style {
+    NSDictionary<NSString *, id <NSSecureCoding, NSCopying>> *styleDictionary = style.toDictionary;
+    NSAssert(styleDictionary.count > 0, @"toDictionary must return NSDictionary that contain a object at least.");
+
+#ifndef NDEBUG
+    {
+        NSMutableDictionary<NSString *, id <NSCopying, NSSecureCoding>> *styleStore = self.styleStore.mutableCopy;
+        self.styleStore = nil;
+        [styleStore addEntriesFromDictionary:styleDictionary];
+        self.styleStore = styleStore;
+    };
+#endif
+
+    {
+        // process { position: 'fixed' } situation
+        NSMutableDictionary<NSString *, id <NSCopying, NSSecureCoding>> *styleStore = nil;
+        id positionValueObject = styleDictionary[@"position"];
+        if ([positionValueObject isKindOfClass:NSString.class]) {
+            if ([((NSString *) positionValueObject).lowercaseString isEqualToString:@"fixed"]) {
+                styleStore = styleDictionary.mutableCopy;
+                styleDictionary = nil;
+                styleStore[@"position"] = @"absolute";
+                styleDictionary = styleStore.copy;
+                self.hm_isFixedPosition = YES;
+            } else {
+                self.hm_isFixedPosition = NO;
+            }
+        }
+    }
+
+    BOOL oldBoxSizing = self.isBoxSizing;
+    __block NSMutableDictionary<NSString *, id <NSCopying, NSSecureCoding>> *animationDictionary = nil;
+    __block NSMutableDictionary<NSString *, id <NSCopying, NSSecureCoding>> *transitions = nil;
+    [styleDictionary enumerateKeysAndObjectsUsingBlock:^(NSString *key, id obj, BOOL *stop) {
+        if ([self.hm_transitionAnimation.needAnimations.allKeys containsObject:key]) {
+            if (!animationDictionary) {
+                animationDictionary = NSMutableDictionary.dictionary;
+            }
+            animationDictionary[key] = obj;
+
+            return;
+        }
+        // animation
+        if ([self.transitionKeySet containsObject:key]) {
+            if (!transitions) {
+                transitions = NSMutableDictionary.dictionary;
+            }
+            transitions[key] = obj;
+
+            return;
+        }
+        if ([self.flexStyleKeySet containsObject:key]) {
+            // 默认情况下都启用 flexbox 布局
+            [self hm_configureWithTarget:self.hm_renderObject cssAttribute:key value:obj converterManager:HMYogaConfig.defaulfConfig];
+
+            return;
+        }
+
+        [self hm_configureWithTarget:self cssAttribute:key value:obj converterManager:HMAttrManager.sharedManager];
+    }];
+
+    // 1. boxSizing: 'border-box' -> 'none'
+    // 2. boxSizing: 'border-box' && borderStyle: 'none'
+    // => reset layout to undefined(NaN)
+    if ((oldBoxSizing && !self.HMBorderBoxSizing) || (self.HMBorderBoxSizing && self.borderStyle == HMBorderStyleNone)) {
+        // rollback borderWidth style
+        self.hm_renderObject.borderWidth = YGUndefined;
+        self.hm_renderObject.borderBottomWidth = YGUndefined;
+        self.hm_renderObject.borderEndWidth = YGUndefined;
+        self.hm_renderObject.borderLeftWidth = YGUndefined;
+        self.hm_renderObject.borderRightWidth = YGUndefined;
+        self.hm_renderObject.borderStartWidth = YGUndefined;
+        self.hm_renderObject.borderTopWidth = YGUndefined;
+    }
+
+    // 设置样式之后，根据 zIndex 处理 fixed
+    [self hm_processFixedPositionWithContext:[HMJSGlobal.globalObject currentContext:style.context]];
+    // 将动画相关信息记录到 transitionAnimation
+    [self.hm_transitionAnimation addAnimations:animationDictionary];
+    // 初始化 transitionAnimation 对象
+    if (!self.hm_transitionAnimation && transitions.count > 0) {
+        self.hm_transitionAnimation = [[HMTransitionAnimation alloc] initWithTransitions:transitions view:self];
+    }
+    [self hm_markDirty];
+}
+
+- (void)hm_layoutBackgroundColorImageBorderShadowCornerRadius {
+    // 移除父类的 UIView 实现
+}
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
@@ -135,7 +560,6 @@ void updateShadowPathForView(HMView *view) {
             defaultIfNegativeTo(borderWidth, defaultIfNegativeTo(_borderRightWidth, directionAwareBorderRightWidth)),
     };
 }
-
 
 - (UIColor *)backgroundColor {
     return _backgroundColor;
@@ -211,6 +635,7 @@ void updateShadowPathForView(HMView *view) {
     [super hummerSetFrame:frame];
     if (!CGSizeEqualToSize(self.bounds.size, oldSize)) {
         [self.layer setNeedsDisplay];
+        self.gradientLayer.frame = self.bounds;
     }
 }
 
@@ -238,7 +663,7 @@ void updateShadowPathForView(HMView *view) {
     // solve this, we'll need to add a container view inside the main view to
     // correctly clip the subviews.
 
-    // TODO(ChasonTang): 黑夜模式
+    // TODO(ChasonTang): DarkMode
     CGColorRef backgroundColor = _backgroundColor.CGColor;
 //    CGColorRef backgroundColor;
 //    if (@available(iOS 13.0, *)) {
