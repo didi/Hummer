20be645f496fae536132745f36f4269d83c8a3b1
YeQing Fang <49541691+fangyeqing123@users.noreply.github.com>
2021-11-09 15:39:11 +0800

    Feature/20211105 storage (#314)
    * feat: 新增拦截NetWork
    
    * feat: 新增拦截storage
    
    * feat: 新增拦截memory
    
    Co-authored-by: fangyeqing <fangyeqing_v@didiglobal.com>

--- tenon/packages/tenon-dev-tool/src/socket.ts
+++ tenon/packages/tenon-dev-tool/src/socket.ts
@@ -1,15 +1,21 @@
 import { log } from './utils'
+import { storageintercept } from './storageintercept'
+import { memoryintercept } from './memoryintercept'
+import { requestintercept } from './requestintercept'
 
 let ws:any, currentType:string
-export default function (url:string, handlers:any,requestintercept:Function) {
+export default function (url:string, handlers:any) {
   ws = new __GLOBAL__.WebSocket(url);
   // 设置各种监听回调
   ws.onopen = () => {
     log('websocket opened~');
+    storageintercept(ws);
+    memoryintercept(ws);
     requestintercept(ws);
   }
 
   ws.onmessage = (event:any) => {
+    log(JSON.stringify(event))
     let msg = JSON.parse(event.data)
     currentType = msg.type || ''
     if (handlers[msg.method]) {
