27992318ae0ec722885cd8abe8ffd08c7f1c1bfe
xingjingmin <xingjingmin@didiglobal.com>
2022-01-17 15:45:37 +0800

    Delegate

---
+++ android/hummer/src/main/java/com/didi/hummer/delegate/IHummerDelegagte.java
@@ -0,0 +1,14 @@
+package com.didi.hummer.delegate;
+
+import android.view.View;
+
+/**
+ * Hummer页面渲染代理接口类
+ *
+ * Created by Xingjm on 2022-01-17.
+ */
+public interface IHummerDelegagte {
+    void initSDK();
+    View initViewAndRender();
+    boolean onBackPressed();
+}
