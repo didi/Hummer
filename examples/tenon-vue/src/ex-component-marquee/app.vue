645069a0ea523fece331b8342670f0a2c4a0abb5
caoenze <caoenze@didichuxing.com>
2022-01-26 19:23:38 +0800

    feat: tenon vue marquee demo

---
+++ examples/tenon-vue/src/ex-component-marquee/app.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="page">
+    <text class="slogan">{{slogan}}</text>
+    <ex-marquee-view :textArray="textArray"></ex-marquee-view>
+    <text class="slogan">{{slogan}}</text>
+  </view>
+</template>
+<style lang="less" scoped>
+  .page{
+    width: 100%;
+    height: 100%;
+  }
+  .slogan{
+    font-size: 0.42rem;
+  }
+</style>
+
+<script>
+
+export default {
+  pageConfig: {
+    canScroll: false
+  },
+  data(){
+    return {
+      slogan: '~ Hello Tenon ~',
+      textArray: ['1111', '22222', '3333333']
+    }
+  },
+  methods: {
+  }
+}
+</script>
