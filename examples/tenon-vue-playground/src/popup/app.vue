aaac588e69f695c0eb2d14406efbb6c0471c4c6b
YeQing Fang <49541691+fangyeqing123@users.noreply.github.com>
2022-02-16 17:14:27 +0800

    Feature/20220111 hummer playground (#359)
    * feat: 新增拦截NetWork
    
    * feat: 新增拦截storage
    
    * feat: 新增拦截memory
    
    * feat: 新增dev—tool的soket参数path
    
    * feat: 新增web模拟器元素检查和List、ViewPager节点对接
    
    * refactor: 重构playground兼容web模拟器
    
    * style: 修改图片地址为线上地址
    
    * fix: 修改组件适配web
    
    * refactor: 删除多余console
    
    * feat: 新增canvas的playground和游戏合集页面
    
    * feat: 新增tenon-vue-playground
    
    * feat: 新增tenon-vue-playground
    
    * fix: 新增git提交忽略
    
    * feat: tenonVuePlayGround新增Popup
    
    Co-authored-by: fangyeqing <fangyeqing_v@didiglobal.com>

---
+++ examples/tenon-vue-playground/src/popup/app.vue
@@ -0,0 +1,89 @@
+<template>
+  <view class="page">
+    <CommonPage
+      titleText="Popup"
+      :showDisplayLayout="false"
+      :showOption="false"
+    >
+      <template v-slot:content>
+        <view class="demo-container">
+          <view class="demo-item">
+            <view class="operation-container">
+              <button class="btn" @tap="openDialog(1)">打开弹窗1</button>
+              <button class="btn" @tap="openDialog(2)">打开弹窗2</button>
+            </view>
+          </view>
+        </view>
+        <dialog
+          :show="showPopup"
+          title="PopupOne"
+          @close="closeDialog(1)"
+        ></dialog>
+        <dialog
+          :show="showPopup2"
+          title="PopupTwo"
+          @close="closeDialog(2)"
+        ></dialog>
+      </template>
+    </CommonPage>
+  </view>
+</template>
+<style lang="less" scoped>
+.demo-container {
+  margin-top: 0.2rem;
+  .demo-item {
+    background-color: #ffffff;
+    width: 100%;
+    margin-bottom: 0.2rem;
+    padding: 0.2rem 0;
+    .operation-container {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      .btn {
+        flex: 1;
+        margin: 0 0.2rem;
+        height: 0.8rem;
+        text-align: center;
+        color: white;
+        background: #fa9153;
+        border-radius: 0.1rem;
+        margin-top: 0.1rem;
+      }
+    }
+  }
+}
+</style>
+
+<script>
+import CommonPage from "../component/CommonPage.vue";
+import Dialog from "./components/dialog";
+export default {
+  components: {
+    Dialog,
+    CommonPage,
+  },
+  data() {
+    return {
+      showPopup: true,
+      showPopup2: false,
+    };
+  },
+  methods: {
+    openDialog(type) {
+      if (type === 1) {
+        this.showPopup = true;
+      } else if (type === 2) {
+        this.showPopup2 = true;
+      }
+    },
+    closeDialog(type) {
+      if (type === 1) {
+        this.showPopup = false;
+      } else if (type === 2) {
+        this.showPopup2 = false;
+      }
+    },
+  },
+};
+</script>
---
+++ examples/tenon-vue-playground/src/popup/entry.js
@@ -0,0 +1,6 @@
+import * as Tenon from '@hummer/tenon-vue';
+import ExPopup from '@hummer/vue-plugin-popup';
+import app from './app.vue';
+
+Tenon.register(ExPopup);
+Tenon.render(app);
