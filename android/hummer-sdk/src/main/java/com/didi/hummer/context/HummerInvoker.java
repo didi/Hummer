c3adc1442f7de1dba1d169644ab26d32930caefb
XiaoFeng <bbssyyuui@qq.com>
2022-07-29 18:21:08 +0800

    fix(Android): loadScriptWithUrl支持异步字节码

--- android/hummer-sdk/src/main/java/com/didi/hummer/context/HummerInvoker.java
+++ android/hummer-sdk/src/main/java/com/didi/hummer/context/HummerInvoker.java
@@ -82,10 +82,19 @@ public class HummerInvoker extends BaseInvoker<HMBase> {
                             callback.call(ret);
                         }
                     } else {
-                        Object ret = mHummerContext.evaluateJavaScript(script, url);
-                        ret = makeHummerError(ret);
-                        if (callback != null) {
-                            callback.call(ret);
+                        if (HummerSDK.isSupportBytecode(mHummerContext.getNamespace())) {
+                            mHummerContext.evaluateJavaScriptAsync(script, url, ret -> {
+                                ret = makeHummerError(ret);
+                                if (callback != null) {
+                                    callback.call(ret);
+                                }
+                            });
+                        } else {
+                            Object ret = mHummerContext.evaluateJavaScript(script, url);
+                            ret = makeHummerError(ret);
+                            if (callback != null) {
+                                callback.call(ret);
+                            }
                         }
                     }
                 });
