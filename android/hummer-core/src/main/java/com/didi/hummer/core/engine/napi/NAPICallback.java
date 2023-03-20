84d0a850e42ea74304d4b7fb5ac3f929b4990ad0
realonezhang <realonezhang@didiglobal.com>
2023-03-20 16:15:12 +0800

    feat(Android):增加JS调线程检查

--- android/hummer-core/src/main/java/com/didi/hummer/core/engine/napi/NAPICallback.java
+++ android/hummer-core/src/main/java/com/didi/hummer/core/engine/napi/NAPICallback.java
@@ -1,7 +1,10 @@
 package com.didi.hummer.core.engine.napi;
 
+import androidx.annotation.MainThread;
+
 import com.didi.hummer.core.engine.JSCallback;
 import com.didi.hummer.core.engine.napi.jni.JSEngine;
+import com.didi.hummer.utils.UIThreadUtil;
 
 /**
  * Created by XiaoFeng on 2021/6/29.
@@ -17,12 +20,13 @@ public class NAPICallback extends NAPIValue implements JSCallback {
         protect();
     }
 
+    @MainThread
     @Override
     public Object call(Object... params) {
+        UIThreadUtil.assetOnMainThread();
         if (!isValid()) {
             return null;
         }
-
         return JSEngine.callFunction(context, -1, value, params);
     }
 }
