d15c0d1744e0996717420e763a81604fa835c7cf
AdamCaoQAQ <1209600784@qq.com>
2023-06-13 11:30:42 +0800

    chore(tenon): 新增拓展组件示例

---
+++ examples/tenon-vue/src/ex-component-scroller-plus/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="page">
+    <ex-scroller-plus style="height: 100hm">
+      <text>I am Scroller Text 1!</text>
+      <text>I am Scroller Text 1!</text>
+      <text>I am Scroller Text 1!</text>
+      <text>I am Scroller Text 1!</text>
+      <text>I am Scroller Text 1!</text>
+      <text>I am Scroller Text 1!</text>
+      <text>I am Scroller Text 1!</text>
+      <text>I am Scroller Text 1!</text>
+      <text>I am Scroller Text 1!</text>
+      <text>{{ aaa }}</text>
+    </ex-scroller-plus>
+  </view>
+</template>
+<style lang="less" scoped>
+.page {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+<script>
+export default {
+  pageConfig: {
+    canScroll: false,
+  },
+  data() {
+    return {
+      aaa: 1111,
+    };
+  },
+};
+</script>
---
+++ examples/tenon-vue/src/ex-component-scroller-plus/entry.js
@@ -0,0 +1,7 @@
+import * as Tenon from '@hummer/tenon-vue';
+import app from './app';
+
+import ScrollerPlus from './scrollerPlus.js';
+Tenon.register(ScrollerPlus);
+
+Tenon.render(app);
