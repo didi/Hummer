7ca3b1c42973e01b8ba02cb79647a7d65256be10
realonezhang <realonezhang@didiglobal.com>
2024-04-03 16:04:36 +0800

    chore(hummer api) __Hummer__ 全局定义

--- api/packages/global.d.ts
+++ api/packages/global.d.ts
@@ -1,2 +1,13 @@
 declare var __GLOBAL__: any
-declare var __Hummer__: any
+declare var __Hummer__: {
+    __globalProxy__?: any;
+    __hummerApi__?: any;
+    __memory__?: any;
+    __navigator__?: any;
+    __notifyCenter__?: any;
+    __dialog__?: any;
+    __request__?: any;
+    __storage__?: any;
+    __toast__?: any;
+    [name: string]: any;
+}
